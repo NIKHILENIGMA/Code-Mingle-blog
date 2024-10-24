74f7a7cf552aa01c6a554e65ae8b6614dad28174
nikhilenigma <nickharmalkar18@gmail.com>
2024-10-24 19:46:43 +0530

    Add tanstack/react-query for API handling

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,9 +1,14 @@
 import AppRouter from "./app/Router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App(): JSX.Element {
+  const queryClient = new QueryClient();
+
   return (
     <>
-      <AppRouter />
+      <QueryClientProvider client={queryClient}>
+        <AppRouter />
+      </QueryClientProvider>
     </>
   );
 }
