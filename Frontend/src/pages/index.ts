4cd81792943890f1ea383d3786129f49c7704075
nikhilenigma <nickharmalkar18@gmail.com>
2024-10-24 19:49:33 +0530

    Add LoginPage, HomePage, AboutPage, and NotFoundPage components to page index

---
+++ Frontend/src/pages/index.ts
@@ -0,0 +1,13 @@
+import HomePage from "./HomePage";
+import AboutPage from "./AboutPage";
+import NotFoundPage from "./NotFoundPage";
+import LoginPage from "./Auth/LoginPage";
+
+
+
+export {
+    HomePage,
+    AboutPage,
+    NotFoundPage,
+    LoginPage
+}
