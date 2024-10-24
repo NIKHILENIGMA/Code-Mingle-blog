a613b2eb71a3f1204ac696ef3dc0aa502448b710
nikhilenigma <nickharmalkar18@gmail.com>
2024-10-24 23:49:37 +0530

    Refactor: Update LoginInput component to use UserTypes module for defining types

--- Frontend/src/features/auth/components/LoginInput.tsx
+++ Frontend/src/features/auth/components/LoginInput.tsx
@@ -1,18 +1,6 @@
 import React from "react";
 import { Input, Label } from "@/components";
-import { FieldError, UseFormRegister } from "react-hook-form";
-
-interface LoginInputProps {
-  type: string;
-  label: string;
-  placeholder: string;
-  register: UseFormRegister<{
-    email: string;
-    password: string;
-  }>;
-  error?: FieldError;
-  name: "email" | "password";
-}
+import { LoginInputProps } from "@/Types/UserTypes";
 
 export const LoginInput: React.FC<LoginInputProps> = ({
   type,
