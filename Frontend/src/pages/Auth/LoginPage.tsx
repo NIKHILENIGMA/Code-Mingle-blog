bc50f54040c4f161b7464100e2e816f2daf1a361
nikhilenigma <nickharmalkar18@gmail.com>
2024-10-25 16:11:02 +0530

    Refactor: Update LoginPage component to use Input and Label components for email and password fields

--- Frontend/src/pages/Auth/LoginPage.tsx
+++ Frontend/src/pages/Auth/LoginPage.tsx
@@ -1,42 +1,57 @@
-import { Button } from "@/components";
-import { LoginInput } from "@/features/auth/components/LoginInput";
+import { Button, Input, Label } from "@/components";
 import { useLoginForm } from "@/features/auth/hooks/useLoginForm";
+import { Link } from "react-router-dom";
 
 function LoginPage(): JSX.Element {
   const { register, handleSubmit, errors, isSubmitting, onSubmit } =
     useLoginForm();
 
   return (
-    <div className="flex items-center justify-center w-full h-screen">
-      <h2>Login Form</h2>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col w-2/3 h-1/2"
-      >
+    <div className="flex flex-col items-center justify-center w-full h-screen ">
+      <h2 className="font-medium uppercase">Login Form</h2>
+      <form onSubmit={handleSubmit(onSubmit)} className="w-2/5 space-y-3 h-1/2">
         {/* Email field */}
-        <LoginInput
+        <div>
+          <Label>Email: </Label>
+          <Input
           type="email"
-          label="Email"
           placeholder="Enter your email"
-          name="email"
-          register={register}
-          error={errors?.email}
-        />
+          id="email"
+          className={errors?.email ? "border-red-500" : "border-gray-300"}
+          {...register("email")}
+          />
+          {errors?.email && <p>{errors.email?.message}</p>}
+        </div>
         {/* Password field */}
-        <LoginInput
+        <div>
+          <Label>Password: </Label>
+          <Input
           type="password"
-          label="Password"
           placeholder="Enter your password"
-          name="password"
-          register={register}
-          error={errors?.password}
-        />
+          id="password"
+          className={errors?.password ? "border-red-500" : "border-gray-300"}
+          {...register("password")}
+          />
+          {errors?.password && <p>{errors.password?.message}</p>}
+        </div>
+        <div className="">
+          <p className="text-sm font-light">
+            Forgot your password?{" "}
+            <Link to="/forgot-password" className="font-normal text-violet-600">Reset it here</Link>
+          </p>
+        </div>
         <div>
           <Button disabled={isSubmitting} type="submit">
             {isSubmitting ? "Loading....." : "Login"}
           </Button>
         </div>
       </form>
+      <div className="">
+        <p className="text-sm font-light">
+          Don't have an account?{" "}
+          <Link to="/signup" className="font-normal text-violet-600">Signup here</Link>
+        </p>  
+      </div>
     </div>
   );
 }
