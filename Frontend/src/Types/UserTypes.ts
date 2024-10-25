9fd6c803484732b4bf81879ad235e7840b7241f5
nikhilenigma <nickharmalkar18@gmail.com>
2024-10-25 16:11:28 +0530

    Refactor: Update LoginInputProps and SignupInputProps in UserTypes.ts

--- Frontend/src/Types/UserTypes.ts
+++ Frontend/src/Types/UserTypes.ts
@@ -1,11 +1,27 @@
 import { FieldError, UseFormRegister } from "react-hook-form";
 
-export type LoginFormInputs = {
-  email: string;
-  password: string;
+export type LoginInputProps = {
+  type: string;
+  label: string;
+  placeholder: string;
+  register: UseFormRegister<{
+    email: string;
+    password: string;
+  }>;
+  error?: FieldError;
+  name: "email" | "password";
 };
 
-export type LoginInputProps = {
-  register: UseFormRegister<{ email: string; password: string }>;
+export type SignupInputProps = {
+  type: string;
+  label: string;
+  placeholder: string;
+  register: UseFormRegister<{
+    email: string;
+    password: string;
+    firstName: string;
+    lastName: string
+  }>;
   error?: FieldError;
+  name: "email" | "password" | "firstName" | "lastName";
 };
