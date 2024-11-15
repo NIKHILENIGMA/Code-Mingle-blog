091eed998f477f1e32643d00314513a734f2dd34
nikhilenigma <nickharmalkar18@gmail.com>
2024-11-15 16:55:22 +0530

    feat: Enhance User model with additional fields and refactor token handling

--- Server/src/Lib/Models/User.ts
+++ Server/src/Lib/Models/User.ts
@@ -1,14 +1,38 @@
-
+// src/models/User.ts
 export interface User {
     id: string
     email: string
+    firstName?: string | null
+    lastName?: string | null
+    username?: string | null
     password: string
+    dob?: Date | null
+    bio?: string | null
+    avatarImg?: string | null
+    coverImg?: string | null
+    role: string
+    createdAt: Date
+    updatedAt: Date
 }
 
-export interface ICreateUser {
+export interface UserDTO {
     tokens: {
         accessToken: string
         refreshToken: string
     }
+    userData: {
+        email: string
+        firstName?: string | null
+        lastName?: string | null
+        username?: string | null
+        dob?: Date | null
+        bio?: string | null
+        avatarImg?: string | null
+        coverImg?: string | null
+        role: string
+    }
+}
+
+export interface ICreateUser {
     newUser: User
 }
