254a7db4ff05d3f48dcf085ae06acd15300843c5
nikhilenigma <nickharmalkar18@gmail.com>
2024-11-15 16:58:05 +0530

    feat: Add Dialog and Popover components to the index for enhanced UI functionality

--- Frontend/src/components/index.ts
+++ Frontend/src/components/index.ts
@@ -1,7 +1,31 @@
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
-import Img from "./Img";
 import { Avatar } from "./ui/avatar";
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+  DialogDescription,
+  DialogHeader,
+  DialogClose,
+} from "./ui/dialog";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import Img from "./Img";
 
-export { Button, Input, Label, Img, Avatar };
+export {
+  Button,
+  Input,
+  Label,
+  Img,
+  Avatar,
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+  DialogDescription,
+  DialogHeader,
+  DialogClose,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+};
