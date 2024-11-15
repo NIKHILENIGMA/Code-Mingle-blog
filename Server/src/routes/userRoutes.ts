449d73e7b554664dc954594130f44f5ee7be61a8
nikhilenigma <nickharmalkar18@gmail.com>
2024-11-15 16:34:40 +0530

    refactor: Remove the unwanted file from the project

--- Server/src/routes/userRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { createUser, updateUser } from '../controllers/userController'
-
-const router = express.Router()
-
-router.post('/create', createUser)
-router.patch('/update/:id', updateUser)
-
-export default router
