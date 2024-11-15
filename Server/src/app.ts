8312052d6a46bc5fda8bc0b001a8a6d33220d1de
nikhilenigma <nickharmalkar18@gmail.com>
2024-11-15 16:50:03 +0530

    refactor: Update app and server initialization with improved middleware and error handling

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -1,46 +1,41 @@
-import express, { Application, NextFunction, Request, Response } from 'express'
-import cors from 'cors'
-import apiRoutes from './routes/apiRoutes'
-import httpResponseMessage from './constant/httpResponseMessage'
-import globalErrorHandler from './middleware/globalErrorHandler'
-import ApiError from './utils/ApiError'
-import helmet from 'helmet'
+// import { cookieParser } from 'cookie-parser';
+import express, { Application } from 'express'
 import routes from './routes'
+import cors from 'cors'
+import helmet from 'helmet'
+import cookieParser from 'cookie-parser'
+import { corsOptions } from './constant/corsOptions'
+// Importing the middleware
+import globalErrorHandler from './middleware/globalErrorHandler'
+import { notFound } from './middleware/NotFound.middleware'
 
 const app: Application = express()
 
-// cors options
-const corsOptions = {
-    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}
 
 // Middlewares
 app.use(helmet()) // for securing the app by setting various HTTP headers
-app.disable('x-powered-by') // for disabling the X-Powered-By header
-app.use(express.json()) // for parsing application/json
+// app.disable('x-powered-by') // for disabling the X-Powered-By header
+app.use(express.json({limit: '16kb'})) // for parsing application/json
+
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+app.use(cookieParser()) // for parsing cookies
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use(express.static('public')) // for serving static files
+
 app.use(cors(corsOptions)) // for enabling cors
 
 // Routes
-app.use('/api/v1', apiRoutes)
-app.use(routes)
+app.use('/api/v1', routes)
 
+// Testing Route
+/* 
+app.get('/', (_req, res: Response) => {
+    res.send('Hello World')
+})
+*/
 
 // 404 Handler
-app.use((_: Request, res: Response, next: NextFunction): void => {
-    res
-      .status(404)
-      .json(
-        new ApiError(
-            httpResponseMessage.error.notFound.code,
-            httpResponseMessage.error.notFound.message,
-        )
-      )
-    next()
-})
+app.use(notFound)
 
 app.use(globalErrorHandler)
 
--- Server/src/server.ts
+++ Server/src/server.ts
@@ -1,43 +1,43 @@
 import app from './app'
 import config from './config/config'
-import logger from './utils/logger'
+import logger, { winstonStream } from './utils/logger'
+import prisma from './Lib/ORMs/PrismaConnection'
 import morgan from 'morgan'
 
 const morganFormat = ':method :url :status :response-time ms'
 
-app.use(
-    morgan(morganFormat, {
-        stream: {
-            write: (message: string) => {
-                const logObj = {
-                    method: message.split(' ')[0],
-                    url: message.split(' ')[1],
-                    status: message.split(' ')[2],
-                    responseTime: message.split(' ')[3]
-                }
-                logger.info(JSON.stringify(logObj))
-            }
-        }
-    })
-)
-const server = app.listen(config.PORT)
+app.use(morgan(morganFormat, { stream: winstonStream })) // Logging HTTP requests
 
-;(() => {
+const server = app.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
+})
+
+// Connect to the database
+void (() => {
     try {
-        logger.info(`APPLICATION STARTED`, {
-            meta: {
-                PORT: config.PORT,
-                SERVER_URL: config.SERVER_URL
-            }
-        })
+        logger.info('Database connection successful')
     } catch (error) {
-        logger.error(error)
-        server.close((err) => {
-            if (err) {
-                logger.error(err)
-            }
-
-            process.exit(1)
-        })
+        logger.error('Error while connecting to the database: ', error)
+        process.exit(1)
     }
 })()
+
+// Handle graceful shutdown
+// Graceful shutdown
+process.on('beforeExit', () => {
+    (async () => {
+        try {
+            await prisma.$disconnect(); // Disconnect Prisma client
+            server.close(); // Close the server
+            logger.info('Server shutting down');
+        } catch (error) {
+            logger.error('Error shutting down server: ', error);
+            process.exit(1); // Exit with failure code
+        }
+    })().catch(err => {
+        logger.error('Error during shutdown process: ', err);
+        process.exit(1); // Exit with failure code
+    });;
+});
+
+export default server
