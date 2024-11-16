a96b96b274e179baa9f033bbbe390a3bd9166425
nikhilenigma <nickharmalkar18@gmail.com>
2024-11-16 12:26:22 +0530

    feat: Add user-related routes for fetching, updating, and managing user profiles and followers

--- Server/src/routes/user.routes.ts
+++ Server/src/routes/user.routes.ts
@@ -1,11 +1,33 @@
 import express from 'express'
-import { createUser } from '../controllers/user.controller'
-// import logger from '../utils/logger';
+import { getAllUsers, getFollowers, getUser, removeUser, updateUser } from '../controllers/user.controller'
 
 const router = express.Router()
 
-router.route('/create').post(createUser);
 
+//? This TypeScript code snippet is setting up routes for a user-related API using Express. 
 
+//@ route is a GET request to fetch a user's details. It calls the getUser controller function.
+router.route('/users/:id').get(getUser)
+
+//@ route is a GET request to fetch all user profiles. It calls the getAllUsers controller function.
+router.route('/users').get(getAllUsers)
+
+//@ route is a PATCH request to update a user's details. It calls the updateUser controller function.
+router.route('/users/:id:').patch(updateUser)
+
+//@ route is a DELETE request to delete a user profile. It calls the removeUser controller function.
+router.route('/users/:id').delete(removeUser)
+
+//@ route is a GET request to fetch a user's followers. It calls the getFollowers controller function.
+router.route('/users/:id/followers').get(getFollowers)
+
+//@ route is a GET request to fetch a user's following. It calls the getFollowing controller function.
+router.route('/users/:id/following').get(getFollowers)
+
+//@ route is a POST request to follow a user. It calls the followUser controller function.
+router.route('/users/:id/follow').post(getFollowers)
+
+//@ route is a DELETE request to unfollow a user. It calls the unfollowUser controller function.
+router.route('/users/:id/unfollow').delete(getFollowers)
 
 export default router
