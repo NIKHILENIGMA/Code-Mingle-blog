11a314ad799060759264e1113c0e02e521729ef1
nikhilenigma <nickharmalkar18@gmail.com>
2024-11-24 00:05:29 +0530

    feat: Add user authentication schemas for signup, login, forgot password, and reset password

--- Server/src/schemas/user.schema.ts
+++ Server/src/schemas/user.schema.ts
@@ -1,9 +1,24 @@
 import { z } from 'zod'
 
-export const createUserSchema = z.object({
+export const signupSchema = z.object({
     firstName: z.string().min(1, 'Name is required'),
     lastName: z.string().min(1, 'Name is required'),
     email: z.string().email('Invalid email address'),
     password: z.string().min(6, 'Password must be at least 6 characters long')
 })
 
+
+export const loginSchema = z.object({
+    email: z.string().email('Invalid email address'),
+    password: z.string().min(6, 'Password must be at least 6 characters long')
+})
+
+
+export const forgotPasswordSchema = z.object({
+    email: z.string().email('Invalid email address')
+})
+
+export const resetPasswordSchema = z.object({
+    newPassword: z.string().min(6, 'Password must be at least 6 characters long'),
+    token: z.string()
+})
