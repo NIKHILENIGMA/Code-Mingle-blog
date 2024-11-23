170ec83eef748b1f5822a293f2b5c1b2ea9679b1
nikhilenigma <nickharmalkar18@gmail.com>
2024-11-24 00:05:08 +0530

    feat: Refactor getUserDetails method to improve error handling and user retrieval

--- Server/src/services/user.service.ts
+++ Server/src/services/user.service.ts
@@ -25,13 +25,17 @@ export default class UserServices {
 
     
     public async getUserDetails(id: string) {
-        const user = await this.UserRepository.getById(id)
-
-        if (!user) {
-            throw new ApiError(404, 'User not found')
+        try {
+            const user = await this.UserRepository.findUserById(id)
+    
+            if (!user) {
+                throw new ApiError(404, 'User not found')
+            }
+    
+            return user
+        } catch (error) {
+            throw new ApiError(500, `Error getting user details ${(error as Error).message}`)
         }
-
-        return user
     }
 
     
