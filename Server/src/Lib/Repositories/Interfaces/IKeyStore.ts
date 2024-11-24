563242e4b82cf4f9b2e5bde5b732b9a37ade5834
nikhilenigma <nickharmalkar18@gmail.com>
2024-11-24 17:56:38 +0530

    feat: Update IKeyStoreRepository to include findByUserId and rename findKeyStoreByUserId to findByRefreshKey

--- Server/src/Lib/Repositories/Interfaces/IKeyStore.ts
+++ Server/src/Lib/Repositories/Interfaces/IKeyStore.ts
@@ -2,6 +2,7 @@ import { KeyStore } from '../../Models/KeyStore'
 import { IBaseRepository } from './IBaseRepository'
 
 export interface IKeyStoreRepository extends IBaseRepository<KeyStore> {
+    findByUserId(userId: string): Promise<KeyStore | null>
     findByAccessKey(accessTokenKey: string): Promise<KeyStore | null>
-    findKeyStoreByUserId(userId: string): Promise<KeyStore | null>
+    findByRefreshKey(refreshTokenKey: string): Promise<KeyStore | null>
 }
