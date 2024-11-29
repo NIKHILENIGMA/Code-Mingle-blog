56dfeb809bf922cb687ea27f78770c1d27bb81b7
nikhilenigma <nickharmalkar18@gmail.com>
2024-11-29 19:17:05 +0530

    refactor: Remove ApiError handling from user service methods

--- Server/src/services/user.service.ts
+++ Server/src/services/user.service.ts
@@ -1,6 +1,6 @@
 import { User } from '../Lib/Models/User'
 import { RepositoryFactory } from '../Lib/Repositories'
-import { ApiError } from '../utils/ApiError'
+// import { ApiError } from '../utils/ApiError'
 import { IUserRepository } from '../Lib/Repositories/Interfaces/IUserRepository'
 // import TokenServices from './token.service'
 
@@ -16,26 +16,12 @@ export default class UserServices {
     public async updateUserDetails(id: string, user: User) {
         const updatedUser = await this.UserRepository.update(id, user)
 
-        if (!updatedUser) {
-            throw new ApiError(404, 'Error while updating user')
-        }
-
         return updatedUser
     }
 
     
     public async getUserDetails(id: string) {
-        try {
-            const user = await this.UserRepository.findUserById(id)
-    
-            if (!user) {
-                throw new ApiError(404, 'User not found')
-            }
-    
-            return user
-        } catch (error) {
-            throw new ApiError(500, `Error getting user details ${(error as Error).message}`)
-        }
+        await this.UserRepository.findUserById(id)
     }
 
     
