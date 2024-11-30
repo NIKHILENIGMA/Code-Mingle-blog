d1730571fd2dc1bb3372d746c90d191b8123fc10
nikhilenigma <nickharmalkar18@gmail.com>
2024-11-30 20:37:12 +0530

    feat: Add AboutPage, HomePage, and NotFoundPage components with basic layout and styling

---
+++ Frontend/src/pages/Public/HomePage.tsx
@@ -0,0 +1,12 @@
+// ../pages/HomePage.js or ../pages/HomePage.tsx
+import React from 'react';
+
+const HomePage: React.FC = () => {
+    return (
+        <div className='w-full h-screen bg-green-300'>
+            <h2 className=''>Hello world</h2>
+        </div>
+    );
+};
+
+export default HomePage;
