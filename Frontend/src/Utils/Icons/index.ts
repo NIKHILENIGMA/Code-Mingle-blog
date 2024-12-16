041e5571198b85ad22ec4bee7750663af77bbdea
nikhilenigma <nickharmalkar18@gmail.com>
2024-12-16 20:00:02 +0530

    feat: Add utility icons for enhanced UI components

---
+++ Frontend/src/Utils/Icons/index.ts
@@ -0,0 +1,79 @@
+import {
+  Upload,
+  ImageIcon,
+  Text,
+  Heading1,
+  Heading2,
+  Heading3,
+  List,
+  ListOrdered,
+  ListTodo,
+  TextQuote,
+  CodeXml,
+  Table,
+  Image,
+  Columns2,
+  SquareMinus,
+  Bold,
+  Italic,
+  Underline,
+  Strikethrough,
+  TypeOutline, // text
+  Code, // code
+  Link, // link
+  Palette, // color
+  Highlighter, // highlight
+  EllipsisVertical, // more
+  AlignLeft, // align left
+  AlignRight, // align right
+  AlignJustify, // align justify
+  AlignCenter, // align center
+  Pilcrow, // Hierarchy
+  ArrowLeftToLine, // make short
+  ArrowRightToLine, // make long
+  Wand, // ai writing 
+  Mic, // voice
+  Eraser, // rewrite
+  SwatchBook, // simplify
+  Languages, // translate
+} from "lucide-react";
+
+export {
+  Upload,
+  ImageIcon,
+  Text,
+  Heading1,
+  Heading2,
+  Heading3,
+  List,
+  ListOrdered,
+  ListTodo,
+  TextQuote,
+  CodeXml,
+  Table,
+  Image,
+  Columns2,
+  SquareMinus,
+  Bold,
+  Wand,
+  Italic,
+  Underline,
+  Strikethrough,
+  TypeOutline, // text
+  Code, // code
+  Link, // link
+  Palette, // color
+  Highlighter, // highlight
+  EllipsisVertical, // more
+  AlignLeft, // align left
+  AlignRight, // align right
+  AlignJustify, // align justify
+  AlignCenter, // align center
+  Pilcrow, // Hierarchy
+  ArrowLeftToLine, // make short
+  ArrowRightToLine, // make long
+  Mic, // voice
+  Eraser, // rewrite
+  SwatchBook, // simplify
+  Languages, // translate
+};
