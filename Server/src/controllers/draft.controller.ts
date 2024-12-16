b16e96f76c8f3d193ff99e8eafb85eca8f134200
nikhilenigma <nickharmalkar18@gmail.com>
2024-12-16 18:31:39 +0530

    feat: Enhance DraftController with create, save, remove, get, and list draft functionalities

--- Server/src/controllers/draft.controller.ts
+++ Server/src/controllers/draft.controller.ts
@@ -1,22 +1,143 @@
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import { AsyncHandler } from '../utils/AsyncHandler'
+import { ISaveDraftRequest, ProtectedRequest } from '../types/app-request'
+import prisma from '../Lib/database/PrismaConnection'
+import { User } from '../Lib/Models/User'
+import { PostStatus } from '@prisma/client'
+import { ApiResponse } from '../utils/ApiResponse'
+import { ApiError } from '../utils/ApiError'
+import responseMessage from '../constant/responseMessage'
+import { BlogDraft } from '../Lib/Models/Blog'
+import DraftService from '../services/draft.service'
 
+const draftService = new DraftService()
 
-export const saveDraft = AsyncHandler(async (_: Request, res: Response) => {
-    // Save draft
-    await new Promise((resolve) => setTimeout(resolve, 1000))
-    res.status(200).send('Draft saved successfully')
-})
-
-
-export const getDraft = AsyncHandler(async (_: Request, res: Response) => {
+export const createDraft = AsyncHandler(async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     // Get draft
-    await new Promise((resolve) => setTimeout(resolve, 1000))
-    res.status(200).send('Draft retrieved successfully')
+    try {
+        const user = (req as ProtectedRequest)?.user as User | undefined
+
+        if (!user) {
+            return ApiError(new Error(responseMessage.MISSING_USER), req, next, 400)
+        }
+
+        const userId: string = (user as unknown as User)?.id
+
+        const draftId = await draftService.newDraftService(req, next, userId)
+
+        return ApiResponse(req, res, 200, 'Draft created successfully', { draftId: draftId })
+    } catch (error) {
+        return ApiError(error instanceof Error ? error : new Error(responseMessage.METHOD_FAILED('create  draft')), req, next, 500)
+    }
 })
 
-export const listDraft = AsyncHandler(async (_: Request, res: Response) => {
+export const saveDraft = AsyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    // Save draft
+    const draftId = req.params.id
+
+    const user = req as ProtectedRequest
+    const userId: string = (user as unknown as User).id
+
+    const { body } = req as ISaveDraftRequest
+
+    if (!draftId) {
+        return ApiError(new Error(responseMessage.MISSING_ID('draft')), req, next, 400)
+    }
+
+    if (!body) {
+        return ApiError(new Error(responseMessage.MISSING_BODY), req, next, 400)
+    }
+
+    try {
+        const draftSaved = await draftService.saveDraftService(req, next, draftId, userId, body)
+
+        if (!draftSaved) {
+            return ApiError(new Error(responseMessage.BAD_REQUEST('draft not saved')), req, next, 400)
+        }
+
+        return ApiResponse(req, res, 200, draftSaved)
+    } catch (error) {
+        return ApiError(error instanceof Error ? error : new Error(responseMessage.METHOD_FAILED('save draft')), req, next, 500)
+    }
+})
+
+export const removeDraft = AsyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const draftId = req.params?.id
+
+    if (!draftId) {
+        return ApiError(new Error(responseMessage.MISSING_ID('draft')), req, next, 400)
+    }
+    const user = req as ProtectedRequest
+
+    if (!user) {
+        return ApiError(new Error(responseMessage.MISSING_USER), req, next, 400)
+    }
+
+    const userId: string = (user as unknown as User)?.id
+
+    try {
+        const removedDraft = await draftService.removeDraftService(req, next, draftId, userId)
+
+        if (!removedDraft) {
+            return ApiError(new Error(responseMessage.BAD_REQUEST('draft not removed')), req, next, 400)
+        }
+
+        return ApiResponse(req, res, 200, removedDraft)
+    } catch (error) {
+        return ApiError(error instanceof Error ? error : new Error(responseMessage.METHOD_FAILED('remove draft')), req, next, 500)
+    }
+})
+
+export const getDraft = AsyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params?.id
+
+    if (!id) {
+        return ApiError(new Error(responseMessage.MISSING_ID('draft')), req, next, 400)
+    }
+
+    const user = req as ProtectedRequest
+
+    if (!user) {
+        return ApiError(new Error(responseMessage.MISSING_USER), req, next, 400)
+    }
+
+    const userId: string = (user as unknown as User)?.id
+
+    try {
+        const draft: Partial<BlogDraft> | void = await draftService.getDraftService(req, next, id, userId)
+
+        if (!draft || draft === undefined) {
+            return ApiError(new Error(responseMessage.NOT_FOUND('draft')), req, next, 404)
+        }
+
+        return ApiResponse(req, res, 200, 'Draft fetched successfully', { draft: draft })
+    } catch (error) {
+        return ApiError(error instanceof Error ? error : new Error(responseMessage.METHOD_FAILED('get draft')), req, next, 500)
+    }
+})
+
+export const listDraft = AsyncHandler(async (req: Request, res: Response, next: NextFunction) => {
     // List drafts
-    await new Promise((resolve) => setTimeout(resolve, 1000))
-    res.status(200).send('Drafts listed successfully')
+    const user = req as ProtectedRequest
+    const userId: string = (user as unknown as User).id
+    try {
+        const getAllDraft = await prisma.post.findMany({
+            where: {
+                status: PostStatus.DRAFT,
+                authorId: userId
+            },
+            orderBy: {
+                createdAt: 'desc'
+            },
+            select: {
+                id: true,
+                title: true,
+                createdAt: true
+            }
+        })
+
+        return ApiResponse(req, res, 200, 'Drafts fetched successfully', getAllDraft)
+    } catch (error) {
+        return ApiError(error instanceof Error ? error : new Error(responseMessage.METHOD_FAILED('list draft')), req, next, 500)
+    }
 })
