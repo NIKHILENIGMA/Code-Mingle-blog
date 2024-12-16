b1a702198668719b8c5e6a38c5c6eaa2669e941d
nikhilenigma <nickharmalkar18@gmail.com>
2024-12-16 19:59:24 +0530

    refactor: Comment out blog post routes and update draft routes path

--- Server/src/routes/blog.routes.ts
+++ Server/src/routes/blog.routes.ts
@@ -1,8 +1,9 @@
 import express from 'express'
-import { createBlogPost, deleteBlogPost, listAllPosts, readBlogPost, searchBlogPosts, updateBlogPost } from '../controllers/blog.controller'
+// import { createBlogPost, deleteBlogPost, listAllPosts, readBlogPost, searchBlogPosts, updateBlogPost } from '../controllers/blog.controller'
 
 const router = express.Router()
 
+/*
 router.route('/write').post(createBlogPost)
 
 router.route('/:postId').patch(updateBlogPost)
@@ -14,5 +15,6 @@ router.route('/:postId').get(readBlogPost)
 router.route('/').get(listAllPosts)
 
 router.route('/search').post(searchBlogPosts)
+*/
 
 export default router
