f6dff40c9b84e6da7abe33e801dd31e7ad46515c
nikhilenigma <nickharmalkar18@gmail.com>
2024-12-16 18:31:52 +0530

    feat: Add routes for creating, removing, and saving drafts with authentication middleware

--- Server/src/routes/draft.routes.ts
+++ Server/src/routes/draft.routes.ts
@@ -1,12 +1,27 @@
 import express from 'express'
-import { getDraft, listDraft, saveDraft } from '../controllers/draft.controller'
+import { createDraft, getDraft, listDraft, removeDraft, saveDraft } from '../controllers/draft.controller'
+import { isAuthenticated } from '../middleware/authentication.middleware'
+// import { validateSchema } from './../middleware/validateSchema.middleware';
+// import { draftContentSchema } from '../schemas/blog.schema';
 
 const router = express.Router()
 
-router.route('/drafts/save').post(saveDraft)
+// Middleware to check if user is authenticated
+// ---------------------------------------------------------------------------------
 
-router.route('/drafts/:draftId').get(getDraft)
+router.use(isAuthenticated)
 
-router.route('/drafts').patch(listDraft)
+// ---------------------------------------------------------------------------------
+
+
+router.route('/newDraft').post(createDraft)
+
+router.route('/:id/save').patch(saveDraft)
+
+router.route('/remove/:id').delete(removeDraft)
+
+router.route('/:id').get(getDraft)
+
+router.route('/').get(listDraft)
 
 export default router
