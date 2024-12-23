a753126b966aea7031b19b03ed5b36484ce70ef3
nikhilenigma <nickharmalkar18@gmail.com>
2024-12-23 23:46:15 +0530

    feat: Enhance validation middleware with separate functions for body, params, and query validation

--- Server/src/middleware/validateSchema.middleware.ts
+++ Server/src/middleware/validateSchema.middleware.ts
@@ -1,15 +1,61 @@
 import { Request, Response, NextFunction } from 'express'
-import { z } from 'zod'
+import { z, AnyZodObject } from 'zod'
 import { ApiError } from '../utils/ApiError'
 
-export const validateSchema = (schema: z.Schema) => {
+/**
+ * Middleware to validate the request body against a schema
+ * @param schema Zod schema to validate the request body against
+ * @returns Middleware function that validates the request body
+ */
+
+export const validateBody = <T extends AnyZodObject>(schema: T) => {
     return (req: Request, _: Response, next: NextFunction) => {
         try {
-            schema.parse(req.body)
+            req.body = schema.parse(req.body) as z.infer<T>;
             next()
         } catch (error) {
             if (error instanceof z.ZodError) {
-                return ApiError(new Error(error.errors[0].message), req, next, 400)
+                return ApiError(new Error(`Schema Error: ${error.errors[0].message}`), req, next, 400)
+            }
+            next(error)
+        }
+    }
+}
+
+/**
+ * Middleware to validate the request params against a schema
+ * @param schema Zod schema to validate the request params against
+ * @returns Middleware function that validates the request params 
+ */
+
+export const validateParams = <T extends AnyZodObject>(schema: T) => {
+    return (req: Request, _: Response, next: NextFunction) => {
+        try {
+            req.params = schema.parse(req.params) as z.infer<T>;
+            next()
+        } catch (error) {
+            if (error instanceof z.ZodError) {
+                return ApiError(new Error(`Schema Error: ${error.errors[0].message}`), req, next, 400)
+            }
+            next(error)
+        }
+    }
+}
+
+/**
+ * Middleware to validate the request query against a schema
+ * @param schema Zod schema to validate the request query against
+ * @returns Middleware function that validates the request query
+ */
+
+export const validateQuery = <T extends AnyZodObject>(schema: T) => {
+    return (req: Request, _: Response, next: NextFunction) => {
+        try {
+            req.query = schema.parse(req.query) as z.infer<T>;
+            next()
+        } catch (error) {
+            if (error instanceof z.ZodError) {
+                return ApiError(new Error(`Schema Error: ${error.errors[0].message}`), req, next, 400)
             }
             next(error)
         }
