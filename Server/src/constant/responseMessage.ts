3c981f5c1b41b8a1f46ffe6382a47399f5741058
nikhilenigma <nickharmalkar18@gmail.com>
2024-12-31 00:13:35 +0530

    feat: Add new response messages for improved error handling in API requests

--- Server/src/constant/responseMessage.ts
+++ Server/src/constant/responseMessage.ts
@@ -8,5 +8,10 @@ export default {
     INVALID_PASSWORD: 'Provided password is incorrect',
     INVALID_TOKEN: 'Provided token is invalid',
     TOKEN_EXPIRED: 'Provided token is expired',
+    MISSING_ID:(message: string) => `Missing id in request params for ${message} operation`,
+    MISSING_USER: 'User not found in request',
+    UNAUTHORIZED: 'Unauthorized request',
+    MISSING_BODY: 'Request body is missing',
+    MISSING_FILE: 'Request file is missing',
     
 }
