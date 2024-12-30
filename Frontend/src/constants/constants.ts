da3c896ad9f92385df9c6d04c037305227228ec6
nikhilenigma <nickharmalkar18@gmail.com>
2024-12-31 00:12:43 +0530

    feat: Add draft URL constant and update navigation items with icons

--- Frontend/src/constants/constants.ts
+++ Frontend/src/constants/constants.ts
@@ -1,8 +1,14 @@
 // Description: All the constants used in the frontend are defined here.
+import {
+  House,
+  ClipboardType,
+  Rss,
+} from "@/Utils/Icons";
 
 export const AUTHENTICATIONURL = "/v1/authentication";
 export const USERURL = "/v1/user";
 export const POSTURL = "/v1/post";
+export const DRAFTURL = "/v1/drafts";
 export const COMMENTURL = "/v1/comment";
 export const LIKEURL = "/v1/like";
 export const FOLLOWURL = "/v1/follow";
@@ -15,8 +21,7 @@ export const SETTINGURL = "/v1/setting";
 export const CONTACTURL = "/v1/contact";
 
 export const NavItems = [
-  { name: "Home", path: "/" },
-  { name: "About", path: "/about" },
-  { name: "Contact", path: "/contact" },
-  { name: "write", path: "/write" },
+  { name: "Home", path: "/", icon: House },
+  { name: "Blogs", path: "/posts", icon: Rss },
+  { name: "Editor", path: "/learn-editor-commands", icon: ClipboardType },
 ];
