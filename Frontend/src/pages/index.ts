384ab9cc9ba9cd4c8900f9ec42ed2c8ae1b9af34
nikhilenigma <nickharmalkar18@gmail.com>
2025-01-07 00:18:42 +0530

    feat: Add new blog-related pages to the main index for improved navigation

--- Frontend/src/pages/index.ts
+++ Frontend/src/pages/index.ts
@@ -4,21 +4,28 @@ import NotFoundPage from "./Public/NotFoundPage";
 import LoginPage from "./Auth/LoginPage";
 import ResetPasswordPage from "./Auth/ForgotPasswordPage";
 import SignupPage from "./Auth/SignupPage";
-import WriteBlogPage from "./Blog/CreatePostPage";
 import ProfilePage from "./ProfileManagement/ProfilePage";
-
-
-
-
-
+import CreatePostPage from "@/pages/Blog/CreatePostPage";
+import DraftPostPage from "@/pages/Blog/DraftPostPage";
+import EditPostPage from "@/pages/Blog/EditPostPage";
+import PreviewPostPage from "@/pages/Blog/ReadPostPage";
+import LearnEditorPage from "@/pages/Blog/LearnEditorPage";
+import ReadPostPage from "@/pages/Blog/ReadPostPage";
+import AllPostsPage from "@/pages/Blog/AllPostsPage";
 
 export {
-    HomePage,
-    AboutPage,
-    NotFoundPage,
-    LoginPage,
-    ResetPasswordPage,
-    SignupPage,
-    WriteBlogPage,
-    ProfilePage
-}
+  HomePage,
+  AboutPage,
+  NotFoundPage,
+  LoginPage,
+  ResetPasswordPage,
+  SignupPage,
+  ProfilePage,
+  CreatePostPage,
+  DraftPostPage,
+  EditPostPage,
+  ReadPostPage,
+  PreviewPostPage,
+  LearnEditorPage,
+  AllPostsPage
+};
