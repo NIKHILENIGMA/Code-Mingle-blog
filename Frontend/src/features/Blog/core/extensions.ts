38916765e2798f99f3caebd8d037bf40b06c41f8
nikhilenigma <nickharmalkar18@gmail.com>
2025-01-07 00:04:15 +0530

    feat: Add custom extensions for Tiptap editor to enhance rich text editing capabilities

---
+++ Frontend/src/features/Blog/core/extensions.ts
@@ -0,0 +1,57 @@
+import { StarterKit } from "@tiptap/starter-kit";
+import Heading from "@tiptap/extension-heading";
+import Placeholder from "@tiptap/extension-placeholder";
+import { SlashCommands } from "./CustomExtensions/SlashCommands";
+import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
+import Underline from "@tiptap/extension-underline";
+import { lowLightConfig } from "./lowLightConfig";
+import { aiWriter } from "./aiWriter";
+import { TextColorExtension } from "./CustomExtensions/TextColorExtension";
+import { TextBackgroundExtension } from "./CustomExtensions/TextBackgroundExtension";
+import { TextStyle } from "./CustomeMarks/TextStyle";
+import FontSizeExtension from "./CustomExtensions/FontSizeExtension";
+import TextAlign from "./CustomNode/TextAlign";
+
+const extensions = [
+  SlashCommands,
+  StarterKit.configure({
+    heading: {
+      levels: [1, 2, 3],
+    },
+  }),
+  Heading,
+  Placeholder.configure({
+    placeholder: ({ node }) => {
+      if (node.type.name === "heading") {
+        switch (node.attrs.level) {
+          case 1:
+            return "Heading 1";
+          case 2:
+            return "Heading 2";
+          case 3:
+            return "Heading 3";
+          default:
+            return "Heading...";
+        }
+      }
+
+      return 'Type "/" for commands';
+    },
+  }),
+  CodeBlockLowlight.configure({
+    lowlight: lowLightConfig,
+    defaultLanguage: "javascript",
+  }),
+  Underline,
+  aiWriter,
+  TextStyle,
+  TextColorExtension,
+  TextBackgroundExtension,
+  FontSizeExtension,
+  TextAlign.configure({
+    types: ["heading", "paragraph"],
+    defaultAlignment: "left",
+  })
+];
+
+export default extensions;
