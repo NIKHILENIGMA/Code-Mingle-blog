1d71aecee37407ae7f26824b0e7587a9ba303643
nikhilenigma <nickharmalkar18@gmail.com>
2025-01-06 23:58:20 +0530

    feat: Add UploadImage component for selecting and dispatching cover images

---
+++ Frontend/src/features/Blog/components/BlogImages/UploadImage.tsx
@@ -0,0 +1,42 @@
+import React, { useRef } from "react";
+import { Button, Input } from "@/components";
+import { useDispatch } from "react-redux";
+import { setCoverImage } from "../../blogSlice";
+
+const UploadImage: React.FC = () => {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const dispatch = useDispatch();
+
+  const handleFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    const url = file ? URL.createObjectURL(file) : null;
+    // const trimUrl = url?.split("blob:")[1] ?? "";
+    if (url) {
+      dispatch(setCoverImage({ coverImageUrl: url }));
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center w-full py-2">
+      <div className="flex items-center justify-center w-full ">
+        <Input
+          id="upload"
+          type="file"
+          ref={fileInputRef}
+          placeholder="Upload Image"
+          onChange={handleFile}
+          className="hidden"
+        />
+        <Button
+          onClick={() => fileInputRef.current?.click()}
+          variant="secondary"
+          className="w-full"
+        >
+          Upload Image
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default UploadImage;
