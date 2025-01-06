8041e17fdfb8f3c1fbb1b8439ca871d53e6fb393
nikhilenigma <nickharmalkar18@gmail.com>
2025-01-07 00:18:29 +0530

    feat: Update NotFoundPage with background image and improved navigation text

--- Frontend/src/pages/Public/NotFoundPage.tsx
+++ Frontend/src/pages/Public/NotFoundPage.tsx
@@ -1,14 +1,21 @@
+import { Img } from "@/components";
 import React from "react";
 import { Link } from "react-router-dom";
 
 const NotFoundPage: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center w-full h-screen space-y-3 bg-slate-500">
-      <h2 className="text-4xl font-medium">404 Not Found</h2>
-      <p>
+    <div className="relative flex flex-col items-center justify-center w-full h-screen space-y-3">
+      <Img
+        src={
+          "https://img.freepik.com/free-vector/glitch-error-404-page-background_23-2148090410.jpg?t=st=1736104469~exp=1736108069~hmac=bac858bcd71f0ff712c67a8136ebfa91cd02cdb6cddf43c8dff68b20fd0ac0f1&w=740"
+        }
+        cn="w-[80%] h-[80%] object-cover"
+        alt="404"
+      />
+      <p className="absolute bottom-10">
         Go to{" "}
         <Link to="/" className="text-blue-400 underline">
-          Home{" "}
+          Go back to Home{" "}
         </Link>
       </p>
     </div>
