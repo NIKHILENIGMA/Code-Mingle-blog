05a0170cc2afd998383f0ff375b33d4e30065986
nikhilenigma <nickharmalkar18@gmail.com>
2025-01-09 17:49:13 +0530

    feat: Add draft reducer to store for improved blog management

--- Frontend/src/app/store/store.ts
+++ Frontend/src/app/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../../features/auth/authSlice";
-import blogReducer from "../../features/Blog/blogSlice";
+import blogReducer from "../../features/Blog/slices/blogSlice";
+import draftReducer from "../../features/Blog/slices/draftSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     blog: blogReducer,
+    draft: draftReducer,
   },
 });
 
