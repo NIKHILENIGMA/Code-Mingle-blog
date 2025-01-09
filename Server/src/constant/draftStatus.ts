01fac0c48dd81cf874c882b2126273c858b4aae3
nikhilenigma <nickharmalkar18@gmail.com>
2025-01-10 01:16:01 +0530

    feat: Add DRAFT_STATUS enum to represent draft states

---
+++ Server/src/constant/draftStatus.ts
@@ -0,0 +1,12 @@
+/**
+ * Enum representing the status of a draft.
+ * 
+ * @enum {string}
+ * @readonly
+ */
+
+export enum DRAFT_STATUS {
+    DRAFT = 'DRAFT',
+    PUBLISHED = 'PUBLISHED',
+    ARCHIVED = 'ARCHIVED'
+}
