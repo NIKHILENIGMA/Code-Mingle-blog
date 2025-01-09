8015909d764a530e72341b9809858cce75e670b4
nikhilenigma <nickharmalkar18@gmail.com>
2025-01-10 01:15:44 +0530

    fix: Adjust formatting in IBaseRepository interface for consistency

--- Server/src/Lib/Repositories/Interfaces/IBaseRepository.ts
+++ Server/src/Lib/Repositories/Interfaces/IBaseRepository.ts
@@ -2,7 +2,7 @@
 
 export interface IBaseRepository<T> {
     create(data: Partial<T>): Promise<T>
-    update(where: Record<string, string | number>, data: Partial<T>): Promise<T | null>
+    update(where: Record<string, string | number> , data: Partial<T>): Promise<T | null>
     delete(where: Record<string, string | number>): Promise<void>
 }
 
