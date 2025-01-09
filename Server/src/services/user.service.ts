60a76caf83938ac83876526d965c35bc0d2d0c67
nikhilenigma <nickharmalkar18@gmail.com>
2025-01-09 17:39:45 +0530

    refactor: Simplify user service methods for improved clarity and consistency

--- Server/src/services/user.service.ts
+++ Server/src/services/user.service.ts
@@ -4,7 +4,6 @@ import { RepositoryFactory } from '../Lib/Repositories'
 import { IUserRepository } from '../Lib/Repositories/Interfaces/IUserRepository'
 // import TokenServices from './token.service'
 
-
 export default class UserServices {
     private UserRepository: IUserRepository
 
@@ -12,19 +11,13 @@ export default class UserServices {
         this.UserRepository = RepositoryFactory.UserRepository()
     }
 
-    
     public async updateUserDetails(id: string, user: User) {
-        const updatedUser = await this.UserRepository.update(id, user)
+        const updatedUser = await this.UserRepository.update({ id }, user)
 
         return updatedUser
     }
 
-    
     public async getUserDetails(id: string) {
         await this.UserRepository.findUserById(id)
     }
-
-    
-
-
 }
