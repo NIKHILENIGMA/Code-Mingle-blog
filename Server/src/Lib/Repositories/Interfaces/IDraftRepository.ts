023b4363d5b8bd22eb565eed1af1f17dc1e17c28
nikhilenigma <nickharmalkar18@gmail.com>
2025-01-10 01:15:53 +0530

    feat: Enhance IDraftRepository with new findDrafts method and optional orderBy parameter

--- Server/src/Lib/Repositories/Interfaces/IDraftRepository.ts
+++ Server/src/Lib/Repositories/Interfaces/IDraftRepository.ts
@@ -4,7 +4,8 @@ import { IBaseRepository } from './IBaseRepository'
 
 export interface IDraftRepository extends IBaseRepository<Post> {
     findDraft(where: {id: string, authorId: string}, fields: object): Promise<Partial<Post> | null>
+    findDrafts(): Promise<Post[] | null>
     findDraftById(where: Record<string, string | number>): Promise<Post | null>
     findDraftBySlug(where: Record<string, string | number>): Promise<Post | null>
-    findDraftsByAuthorId(where: Record<string, string | number>, orderBy: Record<string, string>): Promise<Post[] | null>
+    findDraftsByAuthorId(where: Record<string, string | number>, orderBy?: Record<string, string>): Promise<Post[] | null>
 }
