0c55a70708bc44f067d95e48f28895dcfb7841ab
nikhilenigma <nickharmalkar18@gmail.com>
2025-01-10 22:26:05 +0530

    refactor: Enhance useDeleteDraft hook with optimistic updates and improved query management

--- Frontend/src/features/Blog/hooks/useDeleteDraft.ts
+++ Frontend/src/features/Blog/hooks/useDeleteDraft.ts
@@ -1,4 +1,6 @@
-import { deleteDraftService } from "@/services/api/draftServices";
+import { deleteDraftService } from "@/services/api/draftApiServices";
+import { Draft } from "@/Types/draft";
+import queryClient from "@/Utils/queryClient";
 import { useMutation } from "@tanstack/react-query";
 
 export const useDeleteDraft = (id: string) => {
@@ -7,9 +9,39 @@ export const useDeleteDraft = (id: string) => {
     isPending,
     isError,
   } = useMutation({
-    mutationKey: ["deleteDraft"],
+    mutationKey: ["deleteDraft", id],
     mutationFn: async () => {
       await deleteDraftService(id);
+    },
+
+    onMutate: async () => {
+      // Cancel any outgoing refetches
+      await queryClient.cancelQueries({ queryKey: ["drafts"] });
+      // Snapshot the previous value
+      const previousDrafts = queryClient.getQueryData<{ drafts: Draft[] }>([
+        "drafts",
+      ]);
+      // Optimistically update to the new value
+      queryClient.setQueryData<{ drafts: Draft[] }>(["drafts"], (old) => {
+        if (!old) return { drafts: [] };
+        return {
+          drafts: old.drafts.filter((draft: Draft) => draft.id !== id),
+        };
+      });
+
+      // Return the previous value to rollback
+      return { previousDrafts };
+    },
+
+    onError: (_, __, context) => {
+      queryClient.setQueryData<{ drafts: Draft[] }>(
+        ["drafts"],
+        context?.previousDrafts
+      );
+    },
+
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ["drafts"] });
     },
   });
 
