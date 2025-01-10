1f9a5db6bdb6935ea08cde828a785243334df25d
nikhilenigma <nickharmalkar18@gmail.com>
2025-01-10 13:10:18 +0530

    feat: Add ApiResponse type definition for API response structure

---
+++ Frontend/src/Types/app-response.d.ts
@@ -0,0 +1,19 @@
+/**
+ * Represents the structure of an API response.
+ *
+ * @template T - The type of the data contained in the response.
+ *
+ * @property {boolean} success - Indicates whether the API request was successful.
+ * @property {number} statusCode - The HTTP status code of the response.
+ * @property {string} message - A message providing additional information about the response.
+ * @property {Object} data - The data object containing the response payload.
+ * @property {T} data.draft - The actual data returned by the API.
+ */
+export type ApiResponse<T> = {
+  success: boolean;
+  statusCode: number;
+  message: string;
+  data: {
+    draft: T;
+  };
+};
