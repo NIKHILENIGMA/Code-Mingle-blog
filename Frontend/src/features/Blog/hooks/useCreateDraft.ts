573c74e03413715ac6448abc54fbdf0f27a19c05
nikhilenigma <nickharmalkar18@gmail.com>
2025-01-10 22:25:12 +0530

    refactor: Enhance useCreateDraft hook with improved error handling and query management

--- Frontend/src/features/Blog/hooks/useCreateDraft.ts
+++ Frontend/src/features/Blog/hooks/useCreateDraft.ts
@@ -1,7 +1,9 @@
+import { Draft } from "@/Types/draft";
 import { useMutation } from "@tanstack/react-query";
-import { createDraftService } from "@/services/api/draftServices";
-import { useNavigate } from "react-router-dom";
+import queryClient from "@/Utils/queryClient";
+import { createDraftService } from "@/services/api/draftApiServices";
 import { useCreateDraftReturn } from "../types/useCreateDraft.types";
+import { useNavigate } from "react-router-dom";
 
 export const useCreateDraft = (): useCreateDraftReturn => {
   const navigate = useNavigate();
@@ -14,22 +16,53 @@ export const useCreateDraft = (): useCreateDraftReturn => {
   } = useMutation({
     mutationKey: ["newDraft"],
     mutationFn: async () => {
-      const response = await createDraftService();
-      console.log("Created user data: ", response?.data);
-      return response?.data;
+      const axiosResponse = await createDraftService();
+      console.log("Created user data: ", axiosResponse?.data);
+      return axiosResponse?.data;
+    },
+    
+    onMutate: async () => {
+      // Cancel all queries with the key "drafts"
+      await queryClient.cancelQueries({ queryKey: ["drafts"] }); 
+
+      // Snapshot the previous value
+      const previousDrafts = queryClient.getQueryData<{ drafts: Draft[] }>([
+        "drafts",
+      ]);
+      if (!previousDrafts) {
+        return;
+      }
+
+      // Return the previous value to rollback
+      return { previousDrafts };
+    },
+
+    onError: (_, __, context) => {
+      // Rollback to the previous value
+      queryClient.setQueryData<{ drafts: Draft[] }>(
+        ["drafts"],
+        context?.previousDrafts
+      );
+    },
+
+    onSettled: () => {
+      // Invalidate the cache to refetch the data
+      queryClient.invalidateQueries({ queryKey: ["drafts"] });
     },
   });
 
   /// Handle new draft creation
   const handleNewDraft = async () => {
     try {
+      // Create a new draft
       const newDraft = await newDraftMutation();
 
       if (!newDraft) {
         throw new Error("Draft creation failed!");
       } else {
         console.log("New draft created: ", newDraft);
-        navigate(`${newDraft?.draftId}`);
+        const draftId = newDraft?.draft?.id;
+        navigate(`${draftId}`);
       }
     } catch (error) {
       console.error(error);
