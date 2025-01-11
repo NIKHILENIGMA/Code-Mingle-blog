7a025434391af3e953df93e7bd083d7ceaca5526
nikhilenigma <nickharmalkar18@gmail.com>
2025-01-11 12:44:40 +0530

    feat: Add documentation comment to useAuth hook for clarity on token and persistence retrieval

--- Frontend/src/features/auth/hooks/useAuth.ts
+++ Frontend/src/features/auth/hooks/useAuth.ts
@@ -2,6 +2,7 @@ import { RootState } from "@/app/store/store";
 import { useSelector } from "react-redux";
 
 export const useAuth = () => {
+  /// This hook is used to get the token and persist value from the redux store
   const token = useSelector((state: RootState) => state.auth?.accessToken);
 
   const persist = useSelector((state: RootState) => state.auth.persist);
