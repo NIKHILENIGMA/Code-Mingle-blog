122a0017f6db5004d9b31ebbc3a7de886889538a
nikhilenigma <nickharmalkar18@gmail.com>
2025-01-11 20:33:44 +0530

    feat: Update import paths for Header and Footer components in Main layout

--- Frontend/src/Layout/Main.tsx
+++ Frontend/src/Layout/Main.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
-import Header from "./Header";
-import Footer from "./Footer";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 import { Outlet } from "react-router-dom";
 
 const Main: FC = () => {
