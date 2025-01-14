16b6da60582e5eb58ce7371d1c794cc062629416
nikhilenigma <nickharmalkar18@gmail.com>
2025-01-14 11:39:52 +0530

    feat: Remove Loader, NewDraft, useAutoSave, and EditPostPage components to streamline codebase

--- Frontend/src/components/Loader.tsx
+++
@@ -1,9 +0,0 @@
-function Loader(): JSX.Element {
-    return (
-        <div className="flex items-center justify-center h-screen">
-            <div className="w-32 h-32 ease-linear border-8 border-t-8 border-gray-200 rounded-full loader"></div>
-        </div>
-    );
-}
-
-export default Loader;
