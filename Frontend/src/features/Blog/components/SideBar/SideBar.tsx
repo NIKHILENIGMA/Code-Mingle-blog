f3561b06843c45f229ba62772f9d9cd8c3b2d798
nikhilenigma <nickharmalkar18@gmail.com>
2025-01-25 13:15:29 +0530

    feat: Replace input element with custom Input component in Sidebar for consistency

--- Frontend/src/features/Blog/components/SideBar/SideBar.tsx
+++ Frontend/src/features/Blog/components/SideBar/SideBar.tsx
@@ -3,17 +3,18 @@ import React from "react";
 import { useDrafts } from "../../hooks/useDrafts";
 import SideBarNewDraftButton from "./SideBarNewDraftButton";
 import SideBarDraftList from "./SideBarDraftList";
+import { Input } from "@/components";
 
 const Sidebar: React.FC = () => {
   const { drafts, isError, isLoading } = useDrafts();
 
   return (
-    <div className="w-[20vw] sticky top-0 h-screen p-4 bg-gray-100 border-r border-gray-300 sidebar">
+    <div className="w-[20vw] sticky top-0 h-screen p-4 sidebar">
       {/* SideBar new button handle the new draft creation */}
       <SideBarNewDraftButton />
 
       {/* Search bar handle to search user previous draft */}
-      <input
+      <Input
         type="text"
         placeholder="Search drafts..."
         className="w-full p-2 mb-4 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-400"
