8744885a2f17a05ea14b8cdc43c599a8b88a5723
nikhilenigma <nickharmalkar18@gmail.com>
2025-01-25 13:15:35 +0530

    feat: Simplify DraftActions component by removing unused icons and save status logic; add ModeToggle for improved functionality

--- Frontend/src/features/Blog/components/Drafts/DraftActions.tsx
+++ Frontend/src/features/Blog/components/Drafts/DraftActions.tsx
@@ -1,17 +1,12 @@
 import { FC } from "react";
-import {
-  ScanEye,
-  CloudUpload,
-  LoaderCircle,
-  CloudAlert,
-  Save,
-} from "@/Utils/Icons";
+import { ScanEye, Save } from "@/Utils/Icons";
 import { Button } from "@/components";
 import PublishButton from "./PublishButton";
 import { useSelector } from "react-redux";
 import { RootState } from "@/app/store/store";
 import { useAutoSaveDraft } from "../../hooks/useAutoSaveDraft";
 import { useNavigate } from "react-router-dom";
+import { ModeToggle } from "@/components/mode-toggle";
 
 const DraftActions: FC = () => {
   const navigate = useNavigate();
@@ -31,34 +26,10 @@ const DraftActions: FC = () => {
     navigate(`/preview/${selectedDraft?.id}`);
   };
 
-  /// Get the save status from the store
-  const saveStatus = useSelector((state: RootState) => state.draft.saveDraft);
-
   return (
     <div className="fixed top-0 z-10 flex items-center justify-center w-full h-16 backdrop:blur-md ">
       <div className="flex items-center justify-center h-full px-3 space-x-2 md:w-1/4 lg:w-1/3">
-        {saveStatus === "saved" ? (
-          <div className="flex items-center justify-center h-full px-3 space-x-2 md:w-1/4 lg:w-1/3 animate-in">
-            <p className="flex items-center w-32 text-green-500 justify-evenly text-md">
-              <CloudUpload stroke="#00C55E" size={32} /> Saved
-            </p>
-          </div>
-        ) : saveStatus === "saving" ? (
-          <div className="flex items-center justify-center h-full px-3 space-x-2 md:w-1/4 lg:w-1/3">
-            <span className="text-slate-400">
-              <LoaderCircle />
-              Saving...
-            </span>
-          </div>
-        ) : (
-          <div className="flex items-center justify-center h-full px-3 space-x-2 md:w-1/4 lg:w-1/3">
-            <span className="text-red-500">
-              <CloudAlert stroke="#cc0000" fill="#cc0000" />
-              Error
-            </span>
-          </div>
-        )}
-
+        <ModeToggle />
         <Button variant="ghost" onClick={handleSave}>
           <Save /> Save
         </Button>
