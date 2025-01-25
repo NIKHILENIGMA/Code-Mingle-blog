1c9d51ec41a69366b5186c39aea433d8fcfc6610
nikhilenigma <nickharmalkar18@gmail.com>
2025-01-25 13:14:27 +0530

    feat: Refactor user routes to simplify endpoint structure and improve readability

--- Server/src/routes/user.routes.ts
+++ Server/src/routes/user.routes.ts
@@ -1,33 +1,32 @@
 import express from 'express'
 import { getAllUsers, getFollowers, getUser, removeUser, updateUser } from '../controllers/user.controller'
 
-const router = express.Router()
+const router = express.Router({ mergeParams: true })
 
-
-//? This TypeScript code snippet is setting up routes for a user-related API using Express. 
+//? This TypeScript code snippet is setting up routes for a user-related API using Express.
 
 //@ route is a GET request to fetch a user's details. It calls the getUser controller function.
-router.route('/users/:id').get(getUser)
+router.route('/:id').get(getUser)
 
 //@ route is a GET request to fetch all user profiles. It calls the getAllUsers controller function.
-router.route('/users').get(getAllUsers)
+router.route('/').get(getAllUsers)
 
 //@ route is a PATCH request to update a user's details. It calls the updateUser controller function.
-router.route('/users/:id:').patch(updateUser)
+router.route('/:id:').patch(updateUser)
 
 //@ route is a DELETE request to delete a user profile. It calls the removeUser controller function.
-router.route('/users/:id').delete(removeUser)
+router.route('/:id').delete(removeUser)
 
 //@ route is a GET request to fetch a user's followers. It calls the getFollowers controller function.
-router.route('/users/:id/followers').get(getFollowers)
+router.route('/:id/followers').get(getFollowers)
 
 //@ route is a GET request to fetch a user's following. It calls the getFollowing controller function.
-router.route('/users/:id/following').get(getFollowers)
+router.route('/:id/following').get(getFollowers)
 
 //@ route is a POST request to follow a user. It calls the followUser controller function.
-router.route('/users/:id/follow').post(getFollowers)
+router.route('/:id/follow').post(getFollowers)
 
 //@ route is a DELETE request to unfollow a user. It calls the unfollowUser controller function.
-router.route('/users/:id/unfollow').delete(getFollowers)
+router.route('/:id/unfollow').delete(getFollowers)
 
 export default router
