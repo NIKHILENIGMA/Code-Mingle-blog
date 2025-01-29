f581298ed690f150950b30694593d7a65c3b4527
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-01-30 00:21:14 +0530

    feat: Update like routes to use likeStatusOfPost controller for like and unlike actions

--- Server/src/routes/like.routes.ts
+++ Server/src/routes/like.routes.ts
@@ -1,10 +1,10 @@
 import express from 'express'
-import { addLike } from '../controllers/like.controller'
+import { likeStatusOfPost } from '../controllers/like.controller'
 
 const router = express.Router()
 
-router.route('/:postId/like').post(addLike)
+router.route('/:postId/like').post(likeStatusOfPost)
 
-router.route('/:postId/like').delete(addLike)
+router.route('/:postId/like').delete(likeStatusOfPost)
 
 export default router
