b0c6b9881681e6199af886572b45d5b191ba40b6
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-01-30 00:21:43 +0530

    refactor: Enhance Blog model and introduce DTOs for draft creation and updates

--- Server/src/Lib/Models/Blog.ts
+++ Server/src/Lib/Models/Blog.ts
@@ -1,19 +1,33 @@
 import { PostStatus } from '@prisma/client'
-import { User } from './User'
+// import { User } from './User'
 
 export interface Blog {
     id: string
-    title: string
-    content: string
-    status: PostStatus
+    title: string | null
+    content: string | null
+    publishedAt: Date | null
+    slug: string | null
+    metaDescription: string | null
+    image: string | null
+    thumbnailImage: string | null
     authorId: string
-    author: User
     createdAt: Date
     updatedAt: Date
-    publishedAt: Date
-    slug: string
-    metaDescription: string
+}
+
+export interface CreateDraftDTO {
+    title: string
+    content: string
     image: string
+    status: PostStatus
+    authorId: string
+}
+
+export interface UpdateDraftDTO {
+    title: string
+    content: string
+    image: string
+    authorId: string
 }
 
 export interface BlogDraft {
