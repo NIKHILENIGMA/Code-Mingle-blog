d5a7a0b24e003ead4598decee11ad76f090da0ad
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-03-07 00:16:18 +0530

    feat: Add command interface and basic text formatting commands for Tiptap editor

---
+++ Frontend/src/constants/commands.ts
@@ -0,0 +1,268 @@
+import { Editor } from "@tiptap/core";
+import {
+  ArrowLeftToLine,
+  ArrowRightToLine,
+  AudioLines,
+  Columns2,
+  Eraser,
+  Heading1,
+  Heading2,
+  Heading3,
+  Image,
+  List,
+  ListOrdered,
+  ListTodo,
+  Code,
+  Mic,
+  MonitorPlay,
+  SwatchBook,
+  Table,
+  Text,
+} from "@/Utils/Icons";
+
+export interface CommandInterface {
+  title: string;
+  action: ({ editor }: { editor: Editor }) => void;
+  icon: React.ComponentType;
+  group: string;
+  description: string;
+  shortcut: string;
+}
+
+/**
+ * Commands for basic text formatting
+ * @param editor - The editor instance
+ * @returns An array of commands
+ */
+export const BASIC_COMMANDS = [
+  {
+    title: "Paragraph",
+    action: ({ editor }: { editor: Editor }) => {
+      const { from, to } = editor.state.selection;
+      if (from === to) return;
+
+      editor
+        .chain()
+        .focus()
+        .deleteRange({ from, to })
+        .setNode("paragraph")
+        .run();
+    },
+    icon: Text,
+    group: "Basic",
+    description: "Add a paragraph",
+    shortcut: "cntrl + shift + p",
+  },
+  {
+    title: "Heading 1",
+    action: ({ editor }: { editor: Editor }) => {
+      const { from, to } = editor.state.selection;
+      if (from === to) return;
+      editor
+        .chain()
+        .focus()
+        .deleteRange({ from, to })
+        .setHeading({ level: 1 })
+        .run();
+    },
+    icon: Heading1,
+    group: "Basic",
+    description: "Add a heading 1",
+    shortcut: "#",
+  },
+  {
+    title: "Heading 2",
+    action: ({ editor }: { editor: Editor }) => {
+      const { from, to } = editor.state.selection;
+      if (from === to) return;
+      editor
+        .chain()
+        .focus()
+        .deleteRange({ from, to })
+        .setHeading({ level: 2 })
+        .run();
+    },
+    icon: Heading2,
+    group: "Basic",
+    description: "Add a heading 2",
+    shortcut: "##",
+  },
+  {
+    title: "Heading 3",
+    action: ({ editor }: { editor: Editor }) => {
+      const { from, to } = editor.state.selection;
+      if (from === to) return;
+      editor
+        .chain()
+        .focus()
+        .deleteRange({ from, to })
+        .setHeading({ level: 3 })
+        .run();
+    },
+    icon: Heading3,
+    group: "Basic",
+    description: "Add a heading 3",
+    shortcut: "###",
+  },
+  {
+    title: "Code Block",
+    action: ({ editor }: { editor: Editor }) => {
+      const { from, to } = editor.state.selection;
+      if (from === to) return;
+      editor.chain().focus().deleteRange({ from, to }).setCodeBlock().run();
+    },
+    icon: Code,
+    group: "Basic",
+    description: "Add a code block",
+    shortcut: "```",
+  },
+  {
+    title: "Divider",
+    action: ({ editor }: { editor: Editor }) => {
+      const { from, to } = editor.state.selection;
+      if (from === to) return;
+      editor
+        .chain()
+        .focus()
+        .deleteRange({ from, to })
+        .setHorizontalRule()
+        .run();
+    },
+    icon: Columns2,
+    group: "Basic",
+    description: "Add a divider",
+    shortcut: "---",
+  },
+  {
+    title: "Bullet List",
+    action: ({ editor }: { editor: Editor }) => {
+      const { from, to } = editor.state.selection;
+      if (from === to) return;
+      editor.chain().focus().deleteRange({ from, to }).toggleBulletList().run();
+    },
+    icon: List,
+    group: "List",
+    description: "Add a bullet list",
+    shortcut: "cntrl + shift + 8",
+  },
+  {
+    title: "Number List",
+    action: ({ editor }: { editor: Editor }) => {
+      const { from, to } = editor.state.selection;
+      if (from === to) return;
+      editor
+        .chain()
+        .focus()
+        .deleteRange({ from, to })
+        .toggleOrderedList()
+        .run();
+    },
+    icon: ListOrdered,
+    group: "List",
+    description: "Add an ordered list",
+    shortcut: "cntrl + shift + 7",
+  },
+  {
+    title: "Task List",
+    action: ({ editor }: { editor: Editor }) => {
+      const { from, to } = editor.state.selection;
+      if (from === to) return;
+      editor.chain().focus().deleteRange({ from, to }).toggleTaskList().run();
+    },
+    icon: ListTodo,
+    group: "List",
+    description: "Add a task list",
+    shortcut: "cntrl + shift + 9",
+  },
+  {
+    title: "Simplify text",
+    action: ({ editor }: { editor: Editor }) =>
+      editor.chain().focus().setHeading({ level: 1 }).run(),
+    icon: SwatchBook,
+    group: "Artificial Intelligence",
+    description: "Simplify the text",
+    shortcut: "#",
+  },
+  {
+    title: "Spelling & Grammar",
+    action: ({ editor }: { editor: Editor }) =>
+      editor.chain().focus().setHeading({ level: 2 }).run(),
+    icon: Eraser,
+    group: "Artificial Intelligence",
+    description: "Summarize the text",
+    shortcut: "##",
+  },
+  {
+    title: "Make longer",
+    action: ({ editor }: { editor: Editor }) =>
+      editor.chain().focus().setHeading({ level: 3 }).run(),
+    icon: ArrowRightToLine,
+    group: "Artificial Intelligence",
+    description: "Expand the text",
+    shortcut: "###",
+  },
+  {
+    title: "Make shorter",
+    action: ({ editor }: { editor: Editor }) =>
+      editor.chain().focus().setNode("paragraph").run(),
+    icon: ArrowLeftToLine,
+    group: "Artificial Intelligence",
+    description: "Make the text shorter",
+    shortcut: "cntrl + shift + p",
+  },
+  {
+    title: "Change tone",
+    action: ({ editor }: { editor: Editor }) =>
+      editor.chain().focus().setNode("paragraph").run(),
+    icon: Mic,
+    group: "Artificial Intelligence",
+    description: "Change the tone of the text",
+    shortcut: "cntrl + shift + p",
+  },
+  {
+    title: "Image",
+    action: ({ editor }: { editor: Editor }) =>
+      editor.chain().focus().setImage({ src: "" }).run(),
+    icon: Image,
+    group: "Media",
+    description: "Add an image",
+    shortcut: "%img",
+  },
+  {
+    title: "Youtube",
+    action: ({ editor }: { editor: Editor }) =>
+      editor.chain().focus().setYoutubeVideo({ src: "" }).run(),
+    icon: MonitorPlay,
+    group: "Media",
+    description: "Add a youtube video link",
+    shortcut: "%youtube",
+  },
+  {
+    title: "Audio",
+    action: ({ editor }: { editor: Editor }) =>
+      editor.chain().focus().setHeading({ level: 3 }).run(),
+    icon: AudioLines,
+    group: "Media",
+    description: "Add an audio file",
+    shortcut: "%audio",
+  },
+];
+
+/**
+ * Commands for table operations
+ * @param editor - The editor instance
+ * @returns An array of commands
+ */
+
+// Todo: Add the table commands to the editor
+export const TABLE_COMMANDS = [
+  {
+    title: "Table",
+    action: ({ editor }: { editor: Editor }) =>
+      editor.chain().focus().setNode("table").run(),
+    icon: Table,
+    group: "Table",
+    description: "Add a table",
+    shortcut: "%table",
+  },
+];
