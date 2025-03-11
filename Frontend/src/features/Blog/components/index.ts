b47ead371feb336513181bd267be60356865ee47
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-03-11 15:17:02 +0530

    refactor: Update import paths for components in Blog feature for improved structure

--- Frontend/src/features/Blog/components/index.ts
+++ Frontend/src/features/Blog/components/index.ts
@@ -2,59 +2,44 @@ import CoverImagePopover from "./BlogImages/CoverImagePopover";
 import ShowBlogCoverImage from "./BlogImages/ShowBlogCoverImage";
 import UploadImage from "./BlogImages/UploadImage";
 import UploadUnslashImage from "./BlogImages/UploadUnslashImage";
-import ColorPicker from "./BubbleMenu/ColorPicker";
-import CustomBubbleMenu from "./BubbleMenu/CustomBubbleMenu";
-import FontSizeChange from "./BubbleMenu/FontSizeChange";
-import SelectToolOption from "./BubbleMenu/SelectToolOption";
-import ToolbarButton from "./BubbleMenu/ToolbarButton";
+import CustomBubbleMenu from "../../editor/components/BubbleMenu";
+import ToolbarButton from "../../editor/components/BubbleMenu/ToolbarButton";
 import PostCard from "./Cards/PostCard";
-// import CommentButton from "./Comments/CommentButton";
 import ReplyComment from "./Comments/ReplyComment";
 import DraftActions from "./Drafts/DraftActions";
 import DraftForm from "./Drafts/DraftForm";
 import DraftTitle from "./Drafts/DraftTitle";
-// import NewDraft from "./Drafts/";
 import PublishButton from "./Drafts/PublishButton";
 import PublishDraft from "./Drafts/PublishDraft";
 import SearchBar from "./Drafts/SearchBar";
 import SharePostLink from "./Drafts/SharePostLink";
-import AIWriterNodeView from "./Editor/AIWriterNodeView";
-import SlashCommandsMenu from "./Editor/SlashCommandsMenu";
-import TiptapEditor from "./Editor/TipTapEditor";
+import TiptapEditor from "../../editor/components/TiptapEditor/TipTapEditor";
 import SideBar from "./SideBar/SideBar";
 import SideBarDraftDropDownActions from "./SideBar/SideBarDraftDropDownActions";
 import SideBarMyDrafts from "./SideBar/SideBarMyDrafts";
 import SideBarSuggested from "./SideBar/SideBarSuggested";
 import SideBarToggleButton from "./SideBar/SideBarToggleButton";
 
-
 export {
-    CoverImagePopover,
-    ShowBlogCoverImage,
-    UploadImage,
-    UploadUnslashImage,
-    ColorPicker,
-    CustomBubbleMenu,
-    FontSizeChange,
-    SelectToolOption,
-    ToolbarButton,
-    PostCard,
-    // CommentButton,
-    ReplyComment,
-    DraftActions,
-    DraftForm,
-    DraftTitle,
-    // NewDraft,
-    PublishButton,
-    PublishDraft,
-    SearchBar,
-    SharePostLink,
-    AIWriterNodeView,
-    SlashCommandsMenu,
-    TiptapEditor,
-    SideBar,
-    SideBarDraftDropDownActions,
-    SideBarMyDrafts,
-    SideBarSuggested,
-    SideBarToggleButton,
-}
+  CoverImagePopover,
+  ShowBlogCoverImage,
+  UploadImage,
+  UploadUnslashImage,
+  CustomBubbleMenu,
+  ToolbarButton,
+  PostCard,
+  ReplyComment,
+  DraftActions,
+  DraftForm,
+  DraftTitle,
+  PublishButton,
+  PublishDraft,
+  SearchBar,
+  SharePostLink,
+  TiptapEditor,
+  SideBar,
+  SideBarDraftDropDownActions,
+  SideBarMyDrafts,
+  SideBarSuggested,
+  SideBarToggleButton,
+};
