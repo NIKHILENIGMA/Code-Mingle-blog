926c11ae9dddc80ba66dbdafd13f46a75d202e3b
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-03-31 21:15:28 +0530

    feat: Remove unused draft hooks to streamline codebase

--- Frontend/src/features/drafts/hooks/useImage.ts
+++
@@ -1,9 +0,0 @@
-import { RootState } from "@/app/store/store";
-import { useSelector } from "react-redux";
-
-export const useImage = () => {
-  const blogCoverImg = useSelector(
-    (state: RootState) => state.blog?.coverImageUrl
-  );
-  return { blogCoverImg };
-};
