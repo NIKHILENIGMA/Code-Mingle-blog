61b4d890ad9fe704560e928792c269348b1e561c
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-04-02 01:18:23 +0530

    feat: Integrate DeleteDialog for draft deletion confirmation in SideBarDraftDropDownActions

--- Frontend/src/features/drafts/components/SideBar/SideBarDraftDropDownActions.tsx
+++ Frontend/src/features/drafts/components/SideBar/SideBarDraftDropDownActions.tsx
@@ -4,17 +4,18 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@radix-ui/react-popover";
-import { EllipsisVertical, Trash2, ScanEye } from "@/Utils/Icons";
+import { EllipsisVertical, ScanEye } from "@/Utils/Icons";
 import { Button } from "@/components";
 import { Draft } from "../../types";
 import { useNavigate } from "react-router-dom";
 import { useDraftMutations } from "../../hooks/useDraftMutations";
 import { toast } from "sonner";
+import DeleteDialog from "@/components/DeleteDialog";
 
 const SideBarDraftDropDownActions: FC<{ draft: Draft }> = ({ draft }) => {
   const navigation = useNavigate();
   const { deleteDraftMutation } = useDraftMutations();
-  
+
   const handleDelete = () => {
     deleteDraftMutation.mutate(draft?.id);
     toast.success("Draft deleted successfully", { duration: 3000 });
@@ -38,18 +39,20 @@ const SideBarDraftDropDownActions: FC<{ draft: Draft }> = ({ draft }) => {
 
         {/* Preview and Delete draft */}
         <PopoverContent className="sticky flex flex-col p-2 space-y-2 bg-white dark:bg-[#000700] rounded-md shadow-md">
-          <Button variant="ghost" onClick={() => handlePreview(draft.id)}>
+          <Button
+            variant="ghost"
+            onClick={() => handlePreview(draft.id)}
+            className="border rounded-md hover:text-primary hover:border-primary/65"
+          >
             <ScanEye />
             Preview
           </Button>
-          <Button
-            variant="ghost"
-            onClick={handleDelete}
-            disabled={deleteDraftMutation?.isPending}
-          >
-            <Trash2 />
-            Delete
-          </Button>
+          <DeleteDialog
+            id={draft?.id}
+            title="Confirm Draft Deletion"
+            description="By deleting this draft, you will lose all the changes made to it. This action cannot be undone. This will permanently delete the draft."
+            onDelete={handleDelete}
+          />
         </PopoverContent>
       </Popover>
     </div>
