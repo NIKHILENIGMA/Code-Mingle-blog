320a6f516aeb64ec92bd018e9fb899d3598a2695
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-04-01 17:08:34 +0530

    feat: Add useDraftActions hook for managing draft save and preview actions

---
+++ Frontend/src/features/drafts/hooks/useDraftActions.ts
@@ -0,0 +1,35 @@
+import { useSelector } from "react-redux";
+import { RootState } from "@/app/store/store";
+import { useNavigate } from "react-router-dom";
+import { useDraftMutations } from "./useDraftMutations";
+
+export const useDraftActions = () => {
+  const navigate = useNavigate();
+  const selectedDraft = useSelector(
+    (state: RootState) => state?.draft?.selectedDraft
+  );
+  const { updateDraftMutation } = useDraftMutations();
+
+  /**
+   * * Handles the save action for the draft.
+   *    
+   * @returns {Promise<void>}
+   */
+  const handleSave = async (): Promise<void> => {
+    if (!selectedDraft) return;
+
+    await updateDraftMutation.mutateAsync({
+      id: selectedDraft.id,
+      draft: {
+        title: selectedDraft?.title ?? "",
+        content: selectedDraft?.content ?? "",
+      },
+    });
+  };
+
+  const handlePreview = (): void => {
+    navigate(`/preview/${selectedDraft?.id}`);
+  };
+
+  return { handleSave, handlePreview };
+};
