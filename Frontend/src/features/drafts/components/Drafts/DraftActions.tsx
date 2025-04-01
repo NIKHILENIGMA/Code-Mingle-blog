641c85683b13b87f11d550fd61a8e73b8628dc8a
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-04-01 17:12:11 +0530

    feat: Refactor DraftActions component to use useDraftActions hook for improved action handling

--- Frontend/src/features/drafts/components/Drafts/DraftActions.tsx
+++ Frontend/src/features/drafts/components/Drafts/DraftActions.tsx
@@ -1,47 +1,47 @@
 import { FC } from "react";
-import { ScanEye, Save } from "@/Utils/Icons";
 import { Button } from "@/components";
 import PublishButton from "./PublishButton";
-import { useSelector } from "react-redux";
-import { RootState } from "@/app/store/store";
-import { useAutoSaveDraft } from "../../hooks/useAutoSaveDraft";
-import { useNavigate } from "react-router-dom";
 import { ModeToggle } from "@/components/mode-toggle";
+import { ScanEye, Save } from "@/Utils/Icons";
+import { useDraftActions } from "../../hooks/useDraftActions";
+
+type ActionOptions = {
+  name: string;
+  icon: JSX.Element;
+  action: () => void;
+  variant?: "secondary" | "default";
+};
 
 const DraftActions: FC = () => {
-  const navigate = useNavigate();
-  const selectedDraft = useSelector(
-    (state: RootState) => state?.draft?.selectedDraft
-  );
-  const { saveDraftMutation } = useAutoSaveDraft();
+  const { handleSave, handlePreview } = useDraftActions();
 
-  const handleSave = async () => {
-    if (!selectedDraft) return;
-
-    await saveDraftMutation.mutateAsync(selectedDraft);
-  };
-
-  const handlePreview = () => {
-    navigate(`/preview/${selectedDraft?.id}`);
-  };
+  const actionOptions: ActionOptions[] = [
+    {
+      name: "Preview",
+      icon: <ScanEye size={16} />,
+      action: handlePreview,
+      variant: "secondary",
+    },
+    {
+      name: "Save",
+      icon: <Save size={16} />,
+      action: handleSave,
+      variant: "secondary",
+    },
+  ];
 
   return (
     <div className="flex justify-between items-center mb-4 p-3">
       <div className="flex gap-2">
-        <Button
-          variant="secondary"
-          onClick={handleSave}
-          className=""
-        >
-          <Save size={16} /> Save
-        </Button>
-        <Button
-          variant="secondary"
-          onClick={handlePreview}
-          className=""
-        >
-          <ScanEye /> Preview
-        </Button>
+        {actionOptions.map((action: ActionOptions, index) => (
+          <Button
+            key={action?.name + index}
+            variant={action?.variant}
+            onClick={action?.action}
+          >
+            {action?.icon} {action?.name}
+          </Button>
+        ))}
         <PublishButton />
       </div>
       <ModeToggle />
