f1011725431a3a01dc779fe9c9dd6ffda47ee498
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-04-03 23:55:31 +0530

    feat: Update PreviewMockup to use iframe for draft preview and remove unused code

--- Frontend/src/features/drafts/components/Preview/PreviewMockup.tsx
+++ Frontend/src/features/drafts/components/Preview/PreviewMockup.tsx
@@ -1,38 +1,38 @@
 import { FC } from "react";
 import Loader from "@/components/Loader/Loader";
-import RenderDraftPreview from "./RenderDraftPreview";
+// import RenderDraftPreview from "./RenderDraftPreview";
 
-interface PreviewDraft {
-  preview: {
-    id: string;
-    title: string;
-    slug: string | null;
-    content: string;
-    image: string;
-    readTime: null;
-    category: null;
-    author: {
-      id: string;
-      username: string;
-      avatarImg: string;
-    };
-    createdAt: string;
-    updatedAt: string;
-  };
-}
+// interface PreviewDraft {
+//   preview: {
+//     id: string;
+//     title: string;
+//     slug: string | null;
+//     content: string;
+//     image: string;
+//     readTime: null;
+//     category: null;
+//     author: {
+//       id: string;
+//       username: string;
+//       avatarImg: string;
+//     };
+//     createdAt: string;
+//     updatedAt: string;
+//   };
+// }
 
 interface PreviewMockupProps {
-  selectedDraft: PreviewDraft;
   mockupImage?: string;
   altName?: string;
   mockPlacement?: string;
   placement?: string;
   isLoading?: boolean;
   isError?: boolean;
+  id: string;
 }
 
 const PreviewMockup: FC<PreviewMockupProps> = ({
-  selectedDraft,
+  id,
   mockupImage,
   altName,
   mockPlacement,
@@ -40,16 +40,22 @@ const PreviewMockup: FC<PreviewMockupProps> = ({
   isLoading,
 }) => {
   return (
-    <div className={"relative flex items-center w-3/4 h-full overflow-hidden"}>
+    <div className={"relative flex items-center w-full h-full overflow-hidden py-10 "}>
       <img
         src={mockupImage}
         alt={altName}
-        className={`${mockPlacement} w-full overflow-hidden object-cover `}
+        className={`${mockPlacement}`}
       />
       {isLoading ? (
         <Loader />
       ) : (
-        <RenderDraftPreview styles={placement ?? ""} draft={selectedDraft?.preview} />
+        <iframe
+          src={`http://localhost:5173/posts/preview/${id}`}
+          width="100%"
+          height="600px"
+          className={`${placement}`}
+          allow="autoplay; encrypted-media; picture-in-picture"
+        />
       )}
     </div>
   );
