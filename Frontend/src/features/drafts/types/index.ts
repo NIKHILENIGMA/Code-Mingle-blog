1f966bc81e968b8a1762598f7bd5045c66f3c84d
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-04-03 23:56:09 +0530

    feat: Add PreviewDraft interface to enhance draft handling with preview capabilities

--- Frontend/src/features/drafts/types/index.ts
+++ Frontend/src/features/drafts/types/index.ts
@@ -1,12 +1,29 @@
-export interface Draft  {
-    id: string,
-    title?: string,
-    content?: string,
-    image?: string,
-    tags?: string[],
-    category?: string,
-    thumbnailImage?: string,
-    isPublished?: boolean,
-    publishedAt?: string,
-    updatedAt?: string,
+export interface Draft {
+  id: string;
+  title?: string;
+  content?: string;
+  image?: string;
+  tags?: string[];
+  category?: string;
+  thumbnailImage?: string;
+  isPublished?: boolean;
+  publishedAt?: string;
+  updatedAt?: string;
+}
+
+export interface PreviewDraft {
+  id: string;
+  title: string;
+  slug: string | null;
+  content: string;
+  image: string;
+  readTime: null;
+  category: null;
+  author: {
+    id: string;
+    username: string;
+    avatarImg: string;
+  };
+  createdAt: string;
+  updatedAt: string;
 }
