68c4adc78fca5961900c4f4ff6fe1dd5ca5e2d4e
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-04-03 23:55:37 +0530

    refactor: Improve usePreview hook structure and variable naming for clarity

--- Frontend/src/features/drafts/hooks/usePreview.ts
+++ Frontend/src/features/drafts/hooks/usePreview.ts
@@ -1,34 +1,35 @@
+import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { previewDraftService } from "@/services/api/draftApiServices";
-import { useState } from "react";
+
 
 type DeviceType = "laptop" | "mobile";
 
 export const usePreview = (id: string) => {
-    const [device, setDevice] = useState<DeviceType>("laptop");
-    const handleDeviceChange = (deviceType: DeviceType) => {
-      setDevice(deviceType);
-    };
-    
-    const {
-      data: DraftPreview,
-      isLoading,
-      isError,
-    } = useQuery({
-      queryKey: ["previewDraft", id], // Unique key for the query
-      queryFn: async () => await previewDraftService(id!),
-      refetchOnWindowFocus: false,
-      enabled: !!id,
-      staleTime: 1000 * 60 * 10, // 5 minutes
-      gcTime: 1000 * 60 * 10, // 5 minutes
-    });
+  
+  const [device, setDevice] = useState<DeviceType>("laptop");
+  const handleDeviceChange = (deviceType: DeviceType) => {
+    setDevice(deviceType);
+  };
 
-    return {
-      DraftPreview,
-      isLoading,
-      isError,
-      device,
-      handleDeviceChange,
-    };
-}
+  const {
+    data: draftPreview,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["previewDraft", id!], // Unique key for the query
+    queryFn: async () => await previewDraftService(id!),
+    refetchOnWindowFocus: false,
+    enabled: !!id,
+    staleTime: 1000 * 60 * 10, // 5 minutes
+    gcTime: 1000 * 60 * 10, // 5 minutes
+  });
 
+  return {
+    device,
+    handleDeviceChange,
+    draftPreview,
+    isLoading,
+    isError,
+  };
+};
