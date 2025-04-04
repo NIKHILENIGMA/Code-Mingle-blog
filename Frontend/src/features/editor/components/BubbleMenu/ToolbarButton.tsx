ad5a7b4f54e913d84aa9dbbc42ae18564b867f61
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-04-04 17:42:10 +0530

    fix: Update ToolbarButton styles for improved visibility and interaction

--- Frontend/src/features/editor/components/BubbleMenu/ToolbarButton.tsx
+++ Frontend/src/features/editor/components/BubbleMenu/ToolbarButton.tsx
@@ -17,8 +17,8 @@ const ToolbarButton: React.FC<ToolbarButtonProps> = ({
     <Button
       variant={"link"}
       onClick={onClick}
-      className={`p-2 flex-1 text-gray-300 hover:text-white hover:bg-[#F1F5F9] dark:hover:bg-[#0E0A06] rounded-md transition ${
-        isActive ? "bg-orange-400 rounded-lg p-2 text-black" : ""
+      className={`p-2 flex-1 text-muted-foreground rounded-md transition ${
+        isActive ? "bg-primary rounded-lg p-2 text-white " : ""
       }`}
     >
       {icon}
