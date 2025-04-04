bcf1bd51082e004ae2183f4f53cb9b92948a50a4
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-04-04 13:26:07 +0530

    fix: Update import path for ThemeProvider to reflect new directory structure

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import AppRouter from "./app/Router";
-import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeProvider } from "@/components/DarkMode/theme-provider";
 import { QueryClientProvider } from "@tanstack/react-query";
 import queryClient from "./Utils/queryClient";
 
