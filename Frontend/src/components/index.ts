cdfd594a329b28a18c93e83c93ff2ba7898b1060
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-04-04 14:54:35 +0530

    fix: Refactor imports in index.ts for better organization and clarity

--- Frontend/src/components/index.ts
+++ Frontend/src/components/index.ts
@@ -1,31 +1,185 @@
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
-import { Avatar } from "./ui/avatar";
+import { Avatar, AvatarImage, AvatarFallback } from "./ui/avatar";
+import { Textarea } from "./ui/textarea";
+import {
+  Select,
+  SelectGroup,
+  SelectValue,
+  SelectTrigger,
+  SelectContent,
+  SelectLabel,
+  SelectItem,
+  SelectSeparator,
+  SelectScrollUpButton,
+  SelectScrollDownButton,
+} from "./ui/select";
+import { Checkbox } from "./ui/checkbox";
+import { Switch } from "./ui/switch";
+import { Skeleton } from "./ui/skeleton";
+import {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+  TooltipProvider,
+} from "./ui/tooltip";
+import {
+  Command,
+  CommandDialog,
+  CommandInput,
+  CommandList,
+  CommandEmpty,
+  CommandGroup,
+  CommandItem,
+  CommandShortcut,
+  CommandSeparator,
+} from "./ui/command";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuCheckboxItem,
+  DropdownMenuRadioItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuGroup,
+  DropdownMenuPortal,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuRadioGroup,
+} from "./ui/dropdown-menu";
 import {
   Dialog,
-  DialogContent,
+  DialogPortal,
+  DialogOverlay,
   DialogTrigger,
-  DialogDescription,
-  DialogHeader,
   DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogFooter,
+  DialogTitle,
+  DialogDescription,
 } from "./ui/dialog";
-import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverAnchor,
+} from "./ui/popover";
+import {
+  Sheet,
+  SheetPortal,
+  SheetOverlay,
+  SheetTrigger,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetFooter,
+  SheetTitle,
+  SheetDescription,
+} from "./ui/sheet";
 import Img from "./common/Img";
+import Card from "./common/Card";
+import DeleteDialog from "./common/DeleteDialog";
+import PrivateRoute from "./common/PrivateRoute";
+import { ModeToggle } from "./DarkMode/mode-toggle";
+import { useTheme, ThemeProvider } from "./DarkMode/theme-provider";
+import SideBarNotRender from "./Error/SideBarNotRender";
+import Loader from "./Loader/Loader";
+import SidebarLoader from "./Loader/SidebarLoader";
+import SpinLoader from "./Loader/SpinLoader";
+import Header from "./Header";
+import Footer from "./Footer";
+
 
 export {
+  Header,
+  Footer,
   Button,
   Input,
   Label,
   Img,
   Avatar,
+  AvatarImage,
+  AvatarFallback,
   Dialog,
-  DialogContent,
+  DialogPortal,
+  DialogOverlay,
   DialogTrigger,
-  DialogDescription,
-  DialogHeader,
   DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogFooter,
+  DialogTitle,
+  DialogDescription,
   Popover,
-  PopoverContent,
   PopoverTrigger,
+  PopoverContent,
+  PopoverAnchor,
+  Card,
+  DeleteDialog,
+  PrivateRoute,
+  ModeToggle,
+  ThemeProvider,
+  useTheme,
+  SideBarNotRender,
+  Select,
+  SelectGroup,
+  SelectValue,
+  SelectTrigger,
+  SelectContent,
+  SelectLabel,
+  SelectItem,
+  SelectSeparator,
+  SelectScrollUpButton,
+  SelectScrollDownButton,
+  Checkbox,
+  Switch,
+  Textarea,
+  Skeleton,
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+  TooltipProvider,
+  Command,
+  CommandDialog,
+  CommandInput,
+  CommandList,
+  CommandEmpty,
+  CommandGroup,
+  CommandItem,
+  CommandShortcut,
+  CommandSeparator,
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuCheckboxItem,
+  DropdownMenuRadioItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuGroup,
+  DropdownMenuPortal,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuRadioGroup,
+  Sheet,
+  SheetPortal,
+  SheetOverlay,
+  SheetTrigger,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetFooter,
+  SheetTitle,
+  SheetDescription,
+  Loader,
+  SidebarLoader,
+  SpinLoader,
 };
