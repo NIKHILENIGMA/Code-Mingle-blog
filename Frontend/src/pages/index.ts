97f2e40e33d55bc9f22a641ab0d7f8eda270e037
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-04-04 17:42:23 +0530

    refactor: Remove unused page imports from index.ts

--- Frontend/src/pages/index.ts
+++ Frontend/src/pages/index.ts
@@ -4,12 +4,8 @@ import NotFoundPage from "./Public/NotFoundPage";
 import LoginPage from "./Auth/LoginPage";
 import ResetPasswordPage from "./Auth/ForgotPasswordPage";
 import SignupPage from "./Auth/SignupPage";
-import CreatePostPage from "@/pages/Blog/CreatePostPage";
-import DraftPostPage from "@/pages/Blog/DraftPostPage";
-// import PreviewPostPage from "@/pages/Blog/ReadPostPage";
 import ReadPostPage from "@/pages/Blog/ReadPostPage";
 import LearnEditorPage from "@/pages/Blog/LearnEditorPage";
-import AllPostsPage from "@/pages/Blog/AllPostsPage";
 
 export {
   HomePage,
@@ -18,10 +14,7 @@ export {
   LoginPage,
   ResetPasswordPage,
   SignupPage,
-  CreatePostPage,
-  DraftPostPage,
   ReadPostPage,
   // PreviewPostPage,
   LearnEditorPage,
-  AllPostsPage
 };
