7860bd73a890b99030592b9b541c9f0dc425b2fb
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-04-09 16:09:45 +0530

    feat: Enhance CustomBubbleMenu with shouldShow logic for visibility control

--- Frontend/src/features/editor/components/BubbleMenu/index.tsx
+++ Frontend/src/features/editor/components/BubbleMenu/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useCallback } from "react";
 import { BubbleMenu, Editor } from "@tiptap/react";
 import {
   Bold,
@@ -15,6 +15,8 @@ import HighlightText from "./HighlightText";
 import LinkButton from "./LinkButton";
 import AISuggestion from "./AISuggestion";
 import ChangeTextColor from "./ChangeTextColor";
+import { EditorState, NodeSelection } from "@tiptap/pm/state";
+// import type { Instance, Props } from "tippy.js";
 
 interface CustomeBubbleMenuProps {
   editor: Editor;
@@ -23,79 +25,94 @@ interface CustomeBubbleMenuProps {
 export type Level = 1 | 2 | 3;
 
 const CustomBubbleMenu: FC<CustomeBubbleMenuProps> = ({ editor }) => {
+  const shouldShow = useCallback(
+    ({ editor, state }: { editor: Editor; state: EditorState }) => {
+      const { from, to } = state.selection;
+      const isTextSelected = from !== to;
+      const isNodeSelection = state.selection instanceof NodeSelection;
+      const isImageNode =
+        isNodeSelection &&
+        (state.selection as NodeSelection).node.type.name === "embbed-image";
+
+      return isTextSelected && !editor.isEmpty && !isImageNode;
+    },
+    []
+  );
+
   return (
     <BubbleMenu
       pluginKey="bubbleMenu"
       editor={editor}
-      tippyOptions={{ duration: 100 }}
-      className="flex items-center space-x-2 px-2 py-1 bg-card shadow-md rounded-md border border-secondary w-[50vw]"
+      tippyOptions={{
+        duration: 100,
+      }}
+      shouldShow={shouldShow}
+      className="flex items-center bg-card border border-secondary p-1 px-2 space-x-1 shadow-lg rounded-md w-full lg:min-w-[750px] overflow-hidden"
     >
-      <div className="bubble-menu space-x-0.5 w-full items-center flex overflow-hidden whitespace-nowrap  text-muted-foreground">
-        <AISuggestion editor={editor} />
+      <AISuggestion editor={editor} />
 
-        <Separator orientation="vertical" />
+      <Separator orientation="vertical" />
 
-        <ChangeNodeType editor={editor} />
+      <ChangeNodeType editor={editor} />
 
-        <Separator orientation="vertical" />
-        {/* Bold the selected text */}
-        <ToolbarButton
-          icon={<Bold className="" />}
-          isActive={editor.isActive("bold")}
-          onClick={() => editor.chain().focus().toggleBold().run()}
-        />
+      <Separator orientation="vertical" />
+      {/* Bold the selected text */}
+      <ToolbarButton
+        icon={<Bold className="" />}
+        isActive={editor.isActive("bold")}
+        onClick={() => editor.chain().focus().toggleBold().run()}
+      />
 
-        {/* Italic the selected text */}
-        <ToolbarButton
-          icon={<Italic className="" />}
-          isActive={editor.isActive("italic")}
-          onClick={() => editor.chain().focus().toggleItalic().run()}
-        />
+      {/* Italic the selected text */}
+      <ToolbarButton
+        icon={<Italic className="" />}
+        isActive={editor.isActive("italic")}
+        onClick={() => editor.chain().focus().toggleItalic().run()}
+      />
 
-        {/* Underline the selected text */}
-        <ToolbarButton
-          icon={<UnderLineIcon className="" />}
-          isActive={editor.isActive("underline")}
-          onClick={() => {
-            if (!editor) return null;
-            editor.chain().focus().toggleUnderline().run();
-          }}
-        />
+      {/* Underline the selected text */}
+      <ToolbarButton
+        icon={<UnderLineIcon className="" />}
+        isActive={editor.isActive("underline")}
+        onClick={() => {
+          if (!editor) return null;
+          editor.chain().focus().toggleUnderline().run();
+        }}
+      />
 
-        {/* Strike the selected text */}
-        <ToolbarButton
-          icon={<Strikethrough className="" />}
-          isActive={editor.isActive("strike")}
-          onClick={() => editor.chain().focus().toggleStrike().run()}
-        />
+      {/* Strike the selected text */}
+      <ToolbarButton
+        icon={<Strikethrough className="" />}
+        isActive={editor.isActive("strike")}
+        onClick={() => editor.chain().focus().toggleStrike().run()}
+      />
 
-        {/* Add code to the text */}
-        <ToolbarButton
-          icon={<CodeXml className="" />}
-          isActive={editor.isActive("code")}
-          onClick={() => editor.chain().focus().toggleCode().run()}
-        />
+      {/* Add code to the text */}
+      <ToolbarButton
+        icon={<CodeXml className="" />}
+        isActive={editor.isActive("code")}
+        onClick={() => editor.chain().focus().toggleCode().run()}
+      />
 
-        {/* Add code block to the text */}
-        <ToolbarButton
-          icon={<SquareChartGantt className="" />}
-          isActive={editor.isActive("codeBlock")}
-          onClick={() => editor.chain().focus().toggleCodeBlock().run()}
-        />
+      {/* Add code block to the text */}
+      <ToolbarButton
+        icon={<SquareChartGantt className="" />}
+        isActive={editor.isActive("codeBlock")}
+        onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+      />
 
-        <Separator orientation="vertical" />
+      <Separator orientation="vertical" />
 
-        <LinkButton editor={editor} />
+      <LinkButton editor={editor} />
 
-        <Separator orientation="vertical" />
+      <Separator orientation="vertical" />
 
-        <HighlightText editor={editor} />
+      <HighlightText editor={editor} />
 
-        <Separator orientation="vertical" />
-        
-        <ChangeTextColor editor={editor} />
-        {/* Change the text color */}
-      </div>
+      <Separator orientation="vertical" />
+
+      <ChangeTextColor editor={editor} />
+      {/* Change the text color */}
     </BubbleMenu>
   );
 };
