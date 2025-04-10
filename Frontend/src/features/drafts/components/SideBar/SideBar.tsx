e3a525cbbabb683053e5f220635a93c9f2c1fde4
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-04-10 20:09:52 +0530

    feat: Enhance Sidebar component with search functionality and improved layout

--- Frontend/src/features/drafts/components/SideBar/SideBar.tsx
+++ Frontend/src/features/drafts/components/SideBar/SideBar.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { ChangeEvent, FC, useEffect, useState } from "react";
 import { useDraftQuery } from "@/features/drafts/hooks/useDraftQuery";
 import SideBarNewDraftButton from "./SideBarNewDraftButton";
 import SideBarDraftList from "./SideBarDraftList";
@@ -6,17 +6,50 @@ import { ArrowLeftToLine } from "@/Utils/Icons";
 import { Separator } from "@/components/ui/separator";
 import { useNavigate } from "react-router-dom";
 import { Button, Input } from "@/components";
+import { useDebounce } from "@/hooks/useDebounce";
+import { Draft } from "../../types";
 
-const Sidebar: FC = () => {
+interface SidebarProps {
+  sidebarOpen: boolean;
+}
+
+const Sidebar: FC<SidebarProps> = ({ sidebarOpen }) => {
+  const navigate = useNavigate();
   const { getDraftsQuery } = useDraftQuery();
   const { data: drafts, isLoading, isError } = getDraftsQuery;
-  
-  const navigate = useNavigate();
+  const [search, setSearch] = useState<string>("");
+  const debouncedSearch = useDebounce(search, 500);
+  const [filterDrafts, setFilterDrafts] = useState<Draft[]>([]);
 
+  const handleCardSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value.toLowerCase());
+  };
+
+  // Filter logic runs after debounce settles
+  useEffect(() => {
+    if (drafts?.data && debouncedSearch) {
+      const filtered: Draft[] = drafts.data.filter(
+        (draft: Draft) =>
+          typeof draft.title === "string" &&
+          draft.title.toLowerCase().includes(debouncedSearch)
+      );
+      setFilterDrafts(filtered);
+    } else {
+      setFilterDrafts([]);
+    }
+  }, [debouncedSearch, drafts]);
+
+  //fixed md:static top-0 left-0 z-40 bg-background min-h-screen md:min-h-full p-4 flex flex-col justify-between border-r border-primary-100 transition-transform duration-300 ease-in-out w-full  ${sidebarOpen ? "translate-x-0 " : "-translate-x-full"}
+  
   return (
-    <div className="w-[18rem] min-h-screen p-4 flex flex-col justify-between border-r border-primary-100">
+    <aside
+      className={`fixed top-0 left-0 z-40 min-h-screen bg-background md:min-h-full p-4 flex flex-col justify-between border-r border-primary-100 w-full sm:w-[40%] md:w-[32%] lg:w-[25%] transform transition-transform duration-500 ease-in-out
+        ${sidebarOpen ? "translate-x-0" : "-translate-x-full"}
+      `}
+    >
+
       {/* Header */}
-      <div className="space-y-4">
+      <div className="">
         <h1 className="text-xl font-bold mb-4">NODEDRAFTS</h1>
         {/* Search Bar */}
 
@@ -24,18 +57,30 @@ const Sidebar: FC = () => {
         <SideBarNewDraftButton />
         {/* Divider */}
         <Separator />
-        <Input
-          type="text"
-          placeholder="Search Drafts"
-          className="w-full px-3 py-2 mb-4 border rounded-md"
-        />
+        <div className="w-full flex items-center justify-between relative">
+          <Input
+            type="text"
+            placeholder="Search Drafts"
+            value={search}
+            onChange={handleCardSearch}
+            className="w-full px-3 py-2 mb-4 border rounded-md"
+          />
+          {search && (
+            <Button
+              variant={"link"}
+              className="absolute inset-y-0 end-2 p-0 hover:no-underline"
+              onClick={() => setSearch("")}
+            >
+              clear
+            </Button>
+          )}
+        </div>
 
         {/* Draft Items */}
         <SideBarDraftList
-          drafts={drafts?.data || null}
+          drafts={debouncedSearch ? filterDrafts : drafts?.data || []}
           isLoading={isLoading}
           isError={isError}
-          filter={"search"}
         />
       </div>
 
@@ -44,7 +89,7 @@ const Sidebar: FC = () => {
         <ArrowLeftToLine size={17} />
         Back to home
       </Button>
-    </div>
+    </aside>
   );
 };
 
