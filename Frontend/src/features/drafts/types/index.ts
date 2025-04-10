5ccfb949f2b5ca4b86660e7d98e46f6000eb115f
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-04-10 20:21:19 +0530

    feat: Update Draft interface to enforce required fields and remove optional properties

--- Frontend/src/features/drafts/types/index.ts
+++ Frontend/src/features/drafts/types/index.ts
@@ -1,14 +1,19 @@
 export interface Draft {
   id: string;
-  title?: string;
-  content?: string;
-  image?: string;
-  tags?: string[];
-  category?: string;
-  thumbnailImage?: string;
-  isPublished?: boolean;
-  publishedAt?: string;
-  updatedAt?: string;
+  title: string;
+  status: "DRAFT";
+  content: string;
+  categoryId: null;
+  collectionId: null;
+  image: string;
+  metaDescription: null;
+  publishedAt: null;
+  slug: null;
+  authorId: string;
+  readTime: null;
+  thumbnailImage: string;
+  createdAt: string;
+  updatedAt: string;
 }
 
 export interface PreviewDraft {
