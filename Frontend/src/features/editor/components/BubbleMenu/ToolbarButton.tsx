84515c113408d4a830852ab1ed652bf4acf83867
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-04-11 17:06:48 +0530

    feat: Add tooltip functionality to ToolbarButton for enhanced user interaction

--- Frontend/src/features/editor/components/BubbleMenu/ToolbarButton.tsx
+++ Frontend/src/features/editor/components/BubbleMenu/ToolbarButton.tsx
@@ -1,28 +1,50 @@
 import React from "react";
-import { Button } from "@/components";
+import {
+  Button,
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components";
 
 interface ToolbarButtonProps {
   onClick: () => void;
   isActive: boolean;
   icon: React.ReactNode;
   styles?: string;
+  content?: string;
+  shortcut?: string;
 }
 
 const ToolbarButton: React.FC<ToolbarButtonProps> = ({
   onClick,
   isActive,
   icon,
+  content = "Tooltip",
+  shortcut = "Ctrl + B",
 }) => {
   return (
-    <Button
-      variant={"link"}
-      onClick={onClick}
-      className={`p-2 flex-1 text-muted-foreground rounded-md transition ${
-        isActive ? "bg-primary rounded-lg p-2 text-white " : ""
-      }`}
-    >
-      {icon}
-    </Button>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            variant={"link"}
+            onClick={onClick}
+            className={`p-2 flex-1 text-muted-foreground rounded-md transition ${
+              isActive ? "bg-primary rounded-lg p-2 text-white " : ""
+            }`}
+          >
+            {icon}
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent side="top" className="z-[9999] space-x-3">
+            <div className="flex flex-col items-center space-y-1">
+            <p className="text-sm font-medium">{content}</p>
+            <span className="text-xs text-muted-foreground">{shortcut}</span>
+            </div>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 };
 
