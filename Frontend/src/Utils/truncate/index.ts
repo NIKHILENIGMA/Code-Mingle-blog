00013b05026d6ead0de406243d553530f506ba62
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-04-12 00:24:56 +0530

    refactor: Improve documentation for truncate function to enhance clarity

--- Frontend/src/Utils/truncate/index.ts
+++ Frontend/src/Utils/truncate/index.ts
@@ -1,3 +1,10 @@
+/**
+ *  Truncate a string to a specified length and append ellipsis if it exceeds that length.
+ * @param text
+ * @param maxLength
+ * @returns  {string} - The truncated string with ellipsis if it exceeds the maxLength.
+ */
+
 const truncate = (text: string, maxLength: number) =>
   text.length > maxLength ? `${text.substring(0, maxLength)}...` : text;
 
