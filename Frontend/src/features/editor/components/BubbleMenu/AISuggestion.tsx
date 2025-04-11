41d4fe5989f1463d0980e8a8e11f499ce94f6519
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-04-12 00:23:47 +0530

    refactor: Simplify AISuggestion component by removing unused imports and commented code

--- Frontend/src/features/editor/components/BubbleMenu/AISuggestion.tsx
+++ Frontend/src/features/editor/components/BubbleMenu/AISuggestion.tsx
@@ -1,9 +1,4 @@
-import {
-  // ChangeEvent,
-  FC,
-  // MouseEvent,
-  // useState,
-} from "react";
+import { FC, useState } from "react";
 import { Editor } from "@tiptap/core";
 import {
   ArrowLeftToLine,
@@ -22,120 +17,19 @@ import {
   BsEmojiSurprise,
   BsEmojiWink,
 } from "react-icons/bs";
-import {
-  Button,
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuPortal,
-  DropdownMenuSeparator,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
-  DropdownMenuTrigger,
-} from "@/components";
+import { Button } from "@/components";
 
 interface AISuggestionProps {
   editor: Editor;
 }
 
-// const aiOptions = [
-//   {
-//     icon: <SwatchBook className="w-5 h-5 mr-3" />,
-//     label: "Simplify",
-//   },
-//   {
-//     icon: <Mic className="w-5 h-5 mr-3" />,
-//     label: "Tone",
-//     subOptions: [
-//       {
-//         icon: <BsEmojiSmile className="w-5 h-5 mr-3" />,
-//         label: "Formal",
-//       },
-//       {
-//         icon: <BsEmojiSmileUpsideDown className="w-5 h-5 mr-3" />,
-//         label: "Informal",
-//       },
-//       {
-//         icon: <BsEmojiGrin className="w-5 h-5 mr-3" />,
-//         label: "Optimistic",
-//       },
-//       {
-//         icon: <BsEmojiSunglasses className="w-5 h-5 mr-3" />,
-//         label: "Friendly",
-//       },
-//       {
-//         icon: <BsEmojiLaughing className="w-5 h-5 mr-3" />,
-//         label: "Assertive",
-//       },
-//       {
-//         icon: <BsEmojiSurprise className="w-5 h-5 mr-3" />,
-//         label: "Curious",
-//       },
-//       {
-//         icon: <BsEmojiWink className="w-5 h-5 mr-3" />,
-//         label: "Persuasive",
-//       },
-//     ],
-//   },
-//   {
-//     icon: <Languages className="w-5 h-5 mr-3" />,
-//     label: "Translation",
-//     subOptions: [
-//       {
-//         icon: <Languages className="w-5 h-5 mr-3" />,
-//         label: "Chinese",
-//       },
-//       {
-//         icon: <Languages className="w-5 h-5 mr-3" />,
-//         label: "English",
-//       },
-//       {
-//         icon: <Languages className="w-5 h-5 mr-3" />,
-//         label: "French",
-//       },
-//       {
-//         icon: <Languages className="w-5 h-5 mr-3" />,
-//         label: "German",
-//       },
-//       {
-//         icon: <Languages className="w-5 h-5 mr-3" />,
-//         label: "Greek",
-//       },
-//       {
-//         icon: <Languages className="w-5 h-5 mr-3" />,
-//         label: "Japanese",
-//       },
-//       {
-//         icon: <Languages className="w-5 h-5 mr-3" />,
-//         label: "Russian",
-//       },
-//       {
-//         icon: <Languages className="w-5 h-5 mr-3" />,
-//         label: "Spanish",
-//       },
-//     ],
-//   },
-//   {
-//     icon: <ArrowRightToLine className="w-5 h-5 mr-3" />,
-//     label: "Make Long",
-//   },
-//   {
-//     icon: <ArrowLeftToLine className="w-5 h-5 mr-3" />,
-//     label: "Make Short",
-//   },
-// ];
-
 const AISuggestion: FC<AISuggestionProps> = ({ editor }) => {
-  // const [menu, setMenu] = useState<boolean>(false);
-  // const [selectedOption, setSelectedOption] = useState<string>("");
-
-  // const handleShowMenu = () => {
-  //   setMenu((prev) => !prev);
-  // };
-
+  const [menu, setMenu] = useState<boolean>(false);
+  const handleShowMenu = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    e.preventDefault();
+    setMenu((prev) => !prev);
+  };
   const aiOptions = [
     {
       name: "Simplify",
@@ -226,74 +120,64 @@ const AISuggestion: FC<AISuggestionProps> = ({ editor }) => {
   ];
 
   const handleOptionSelect = (option: { label: string; type?: string }) => {
-    if (option?.label === "Make Long") {
-      editor.chain().focus().makeLongText().run();
-    }
+    const { label, type } = option;
 
-    if (option?.label === "Make Short") {
-      editor.chain().focus().makeShortText().run();
-    }
+    switch (label) {
+      case "Make Long":
+        editor.chain().focus().makeLongText().run();
+        break;
 
-    if (option?.label === "Simplify") {
-      editor.chain().focus().simplify().run();
-    }
+      case "Make Short":
+        editor.chain().focus().makeShortText().run();
+        break;
 
-    if (option?.type === "Translation") {
-      editor.chain().focus().translateText(option?.label).run();
-    }
+      case "Simplify":
+        editor.chain().focus().simplify().run();
+        break;
 
-    if (option?.type === "Tone") {
-      editor.chain().focus().setTone(option?.label).run();
+      default:
+        if (type === "Translation") {
+          editor.chain().focus().translateText(label).run();
+        } else if (type === "Tone") {
+          editor.chain().focus().setTone(label).run();
+        }
+        break;
     }
-
-    // setMenu(false); // Close the menu after selecting an option
   };
 
-  // const handleValueChange = (value: string) => {
-  //   console.log(value);
-  // };
-
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          onPointerDown={(e) => {
-            e.stopPropagation(); // ✅ prevent editor blur
-          }}
-          variant="outline"
-        >
-          <Wand /> Ask AI
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent
-        onPointerDown={(e) => {
-          e.stopPropagation(); // ✅ again, stop blur trigger
-        }}
-        className="w-56"
+    <>
+      <Button
+        variant={"outline"}
+        onClick={handleShowMenu}
+        className="border-none shadow-none hover:bg-primary/10 hover:text-primary transition-colors"
       >
-        <DropdownMenuLabel>AI Options</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuGroup>
-          {aiOptions.map((option) =>
-            option.subOptions ? (
-              <DropdownMenuSub key={option.value}>
-                <DropdownMenuSubTrigger
-                  onPointerDown={(e) => {
-                    e.stopPropagation(); // ✅ again, stop blur trigger
-                  }}
+        <Wand />
+        Ask AI
+      </Button>
+
+      {menu ? (
+        <div className="absolute w-58 top-full mt-1 left-0 rounded-lg shadow-lg border border-primary/70 bg-background">
+          <ul className="py-2">
+            {aiOptions.map((option, index) => (
+              <li
+                key={index + option.label}
+                className="relative group cursor-pointer"
+              >
+                <div
+                  className="flex items-center px-4 py-2 hover:bg-primary/10 hover:text-primary transition-colors"
+                  onClick={() => handleOptionSelect(option)}
                 >
                   {option.icon}
-                  {option.label}
-                </DropdownMenuSubTrigger>
-                <DropdownMenuPortal>
-                  <DropdownMenuSubContent
-                    onPointerDown={(e) => {
-                      e.stopPropagation(); // ✅ again, stop blur trigger
-                    }}
-                  >
-                    {option.subOptions.map((subOption) => (
-                      <DropdownMenuItem
-                        key={subOption.label}
+                  <span className="text-sm font-medium">{option.label}</span>
+                </div>
+
+                {option?.subOptions && option.subOptions?.length > 0 && (
+                  <ul className="absolute left-full top-0 w-64 rounded-lg shadow-lg border bg-background hidden group-hover:block z-10">
+                    {option.subOptions.map((subOption, subIndex) => (
+                      <li
+                        key={subIndex + subOption.label}
+                        className="flex items-center px-4 py-2 hover:bg-primary/10 hover:text-primary cursor-pointer transition-colors"
                         onClick={() =>
                           handleOptionSelect({
                             type: option.label,
@@ -302,25 +186,19 @@ const AISuggestion: FC<AISuggestionProps> = ({ editor }) => {
                         }
                       >
                         {subOption.icon}
-                        {subOption.label}
-                      </DropdownMenuItem>
+                        <span className="text-sm font-medium">
+                          {subOption.label}
+                        </span>
+                      </li>
                     ))}
-                  </DropdownMenuSubContent>
-                </DropdownMenuPortal>
-              </DropdownMenuSub>
-            ) : (
-              <DropdownMenuItem
-                key={option.value}
-                onClick={() => handleOptionSelect(option)}
-              >
-                {option.icon}
-                {option.label}
-              </DropdownMenuItem>
-            )
-          )}
-        </DropdownMenuGroup>
-      </DropdownMenuContent>
-    </DropdownMenu>
+                  </ul>
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : null}
+    </>
   );
 };
 
@@ -401,54 +279,74 @@ export default AISuggestion;
 //   </SelectContent>
 // </Select>
 
-// <div>
-//   <button
-//     onClick={handleShowMenu}
-//     className="flex items-center justify-center w-full p-2 space-x-2"
-//   >
 //
-//   </button>
 
-//   {menu ? (
-//     <div className="absolute w-58 top-full mt-1 left-0 rounded-lg shadow-lg border border-primary/70 bg-background">
-//       <ul className="py-2">
-//         {aiOptions.map((option, index) => (
-//           <li
-//             key={index + option.label}
-//             className="relative group cursor-pointer"
-//           >
-//             <div
-//               className="flex items-center px-4 py-2 hover:bg-primary/10 hover:text-primary transition-colors"
-//               onClick={() => handleOptionSelect(option)}
-//             >
-//               {option.icon}
-//               <span className="text-sm font-medium">{option.label}</span>
-//             </div>
-
-//             {option?.subOptions && option.subOptions?.length > 0 && (
-//               <ul className="absolute left-full top-0 w-64 rounded-lg shadow-lg border bg-background hidden group-hover:block z-10">
-//                 {option.subOptions.map((subOption, subIndex) => (
-//                   <li
-//                     key={subIndex + subOption.label}
-//                     className="flex items-center px-4 py-2 hover:bg-primary/10 hover:text-primary cursor-pointer transition-colors"
-//                     onClick={() =>
-//                       handleOptionSelect({
-//                         type: option.label,
-//                         label: subOption.label,
-//                       })
-//                     }
-//                   >
-//                     {subOption.icon}
-//                     <span className="text-sm font-medium">
-//                       {subOption.label}
-//                     </span>
-//                   </li>
-//                 ))}
-//               </ul>
-//             )}
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   ) : null}
-// </div>
+{
+  /* <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          onMouseDown={(e) => e.stopPropagation()}
+          onPointerDown={(e) => e.stopPropagation()}
+          variant="outline"
+        >
+          <Wand /> Ask AI
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        onMouseDown={(e) => e.stopPropagation()}
+        onPointerDown={(e) => e.stopPropagation()}
+        className="w-56"
+      >
+        <DropdownMenuLabel>AI Options</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          {aiOptions.map((option) =>
+            option.subOptions ? (
+              <DropdownMenuSub key={option.value}>
+                <DropdownMenuSubTrigger
+                  onMouseDown={(e) => e.stopPropagation()}
+                  onPointerDown={(e) => e.stopPropagation()}
+                >
+                  {option.icon}
+                  {option.label}
+                </DropdownMenuSubTrigger>
+                <DropdownMenuPortal>
+                  <DropdownMenuSubContent
+                    onMouseDown={(e) => e.stopPropagation()}
+                    onPointerDown={(e) => e.stopPropagation()}
+                  >
+                    {option.subOptions.map((subOption) => (
+                      <DropdownMenuItem
+                        key={subOption.label}
+                        onMouseDown={(e) => e.stopPropagation()}
+                        onPointerDown={(e) => e.stopPropagation()}
+                        onClick={() =>
+                          handleOptionSelect({
+                            type: option.label,
+                            label: subOption.label,
+                          })
+                        }
+                      >
+                        {subOption.icon}
+                        {subOption.label}
+                      </DropdownMenuItem>
+                    ))}
+                  </DropdownMenuSubContent>
+                </DropdownMenuPortal>
+              </DropdownMenuSub>
+            ) : (
+              <DropdownMenuItem
+                key={option.value}
+                onMouseDown={(e) => e.stopPropagation()}
+                onPointerDown={(e) => e.stopPropagation()}
+                onClick={() => handleOptionSelect(option)}
+              >
+                {option.icon}
+                {option.label}
+              </DropdownMenuItem>
+            )
+          )}
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu> */
+}
