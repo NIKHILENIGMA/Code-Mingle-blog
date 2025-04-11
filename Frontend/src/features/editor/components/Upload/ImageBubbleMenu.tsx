4a1af8240c534d3a45a3a5252a2bb355b55be8bf
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-04-11 16:15:19 +0530

    feat: Enhance ImageBubbleMenu with copy image URL functionality and UI improvements

--- Frontend/src/features/editor/components/Upload/ImageBubbleMenu.tsx
+++ Frontend/src/features/editor/components/Upload/ImageBubbleMenu.tsx
@@ -6,27 +6,29 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components";
-import { Pencil, Trash2 } from "@/Utils/Icons";
+import { Clipboard, ClipboardCheck, Pencil, Trash2 } from "@/Utils/Icons";
 
 interface ImageBubbleMenuProps {
+  isCopy?: boolean;
   onImageChange: () => void;
   onRemoveImage: () => void;
+  onCopyImageUrl: () => void;
 }
 
 const ImageBubbleMenu: FC<ImageBubbleMenuProps> = ({
+  isCopy = false,
+  onCopyImageUrl,
   onImageChange,
   onRemoveImage,
 }) => {
   //handleImageChange
   return (
-    <div className="absolute z-[9999] top-0 left-[40%] space-x-4 bg-card rounded-sm border border-secondary/50 shadow-lg p-2">
+    <div className="absolute z-[9999] top-0 right-0 space-x-2 rounded-sm border-secondary/50 p-2">
       <TooltipProvider>
         <Tooltip>
           <TooltipTrigger asChild>
-            <Button variant={"secondary"} 
-            onClick={onImageChange}
-            >
-              <Pencil className="w-4 h-4" />
+            <Button variant={"ghost"} className="p-2" onClick={onImageChange}>
+              <Pencil size={10} />
             </Button>
           </TooltipTrigger>
           <TooltipContent side="top" className="z-[9999]">
@@ -38,11 +40,27 @@ const ImageBubbleMenu: FC<ImageBubbleMenuProps> = ({
         <Tooltip>
           <TooltipTrigger asChild>
             <Button
-              variant={"secondary"}
-              onClick={onRemoveImage}
-              className="px-4 py-2 text-sm"
+              variant={"ghost"}
+              className="p-2 hover:text-primary/80"
+              onClick={onCopyImageUrl}
             >
-              <Trash2 color="red" />
+              {isCopy ? <ClipboardCheck /> : <Clipboard />}
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent side="top" className="z-[9999]">
+            <p>Copy Image URL</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button
+              variant={"ghost"}
+              className="p-2 hover:text-primary/80"
+              onClick={onRemoveImage}
+            >
+              <Trash2 />
             </Button>
           </TooltipTrigger>
           <TooltipContent side="top" className="z-[9999]">
