618f65b9e7711a6459e33d34c4c14c36ef0db42a
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-04-13 00:56:33 +0530

    feat: Add PromptType, ToneType, and PromptSelectionProps interfaces for prompt customization

--- Server/src/types/types.ts
+++ Server/src/types/types.ts
@@ -63,3 +63,48 @@ export interface CloundinaryOption {
     resource: 'image' | 'auto' | 'raw' | 'video'
     altName: string
 }
+
+/**
+ * @typedef {Object} PromptType
+ * @property {string} simple - Simple prompt type.
+ * @property {string} advanced - Advanced prompt type.
+ * @property {string} condense - Condense prompt type.
+ */
+export type PromptType = 'simple' | 'advanced' | 'condense'
+/**
+ * @typedef {Object} ToneType
+ * @property {string} Professional - Professional tone.
+ * @property {string} Casual - Casual tone.
+ * @property {string} Friendly - Friendly tone.
+ * @property {string} Formal - Formal tone.
+ * @property {string} Confident - Confident tone.
+ * @property {string} Optimistic - Optimistic tone.
+ * @property {string} Empathetic - Empathetic tone.
+ * @property {string} Assertive - Assertive tone.
+ */
+export type ToneType = 'Professional' | 'Casual' | 'Friendly' | 'Formal' | 'Confident' | 'Optimistic' | 'Empathetic' | 'Assertive'
+
+/**
+ * @typedef {Object} errorResponse
+ * @property {string} error - The error message.
+ * @property {string} message - The error description.
+ */
+export interface errorResponse {
+    error: string
+    message: string
+}
+
+/**
+ * @typedef {Object} PromptSelectionProps
+ * @property {PromptType} type - The type of request (e.g., 'simple', 'advanced', 'condense').
+ * @property {ToneType} tone - The tone of the text (e.g., 'formal', 'informal', 'neutral').
+ * @property {string} text - The text to be processed.
+ * @property {number} [maxLength] - Optional maximum length for the text.
+ */
+
+export interface PromptSelectionProps {
+    type: PromptType
+    tone: ToneType
+    text: string
+    maxLength?: number
+}
