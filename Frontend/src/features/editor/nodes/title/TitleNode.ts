213c4150a92582e70e40a9e9a1a76729e9842864
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-04-16 20:35:27 +0530

    refactor: Clean up TitleNode component by removing unused imports and commented code

--- Frontend/src/features/editor/nodes/title/TitleNode.ts
+++ Frontend/src/features/editor/nodes/title/TitleNode.ts
@@ -1,40 +1,45 @@
-import { CommandProps, Node } from "@tiptap/core";
-import { ReactNodeViewRenderer } from "@tiptap/react";
-import Title from "../../components/Title";
+import { Node } from "@tiptap/core";
+// import { ReactNodeViewRenderer } from "@tiptap/react";
+// import Title from "../../components/Title";
 
-declare module "@tiptap/core" {
-  interface Commands<ReturnType> {
-    title: {
-      setTitle: (title: string) => ReturnType;
-    };
-  }
-}
+// declare module "@tiptap/core" {
+//   interface Commands<ReturnType> {
+//     title: {
+//       setTitle: (title: string) => ReturnType;
+//     };
+//   }
+// }
 
 export const TitleNode = Node.create({
-  name: "title",
-  content: "inline*",
-  group: "block",
-  defining: true,
+  name: 'title',
+
+  group: 'block',
+
+  content: 'inline*',
 
   parseHTML() {
-    return [{ tag: "h1" }];
+    return [
+      {
+        tag: 'h1',
+      },
+    ];
   },
 
   renderHTML({ HTMLAttributes }) {
-    return ["h1", HTMLAttributes, 0];
+    return ['h1', HTMLAttributes, 0];
   },
 
-  addNodeView() {
-    return ReactNodeViewRenderer(Title)
-  },
+  // addNodeView() {
+  //   return ReactNodeViewRenderer(Title)
+  // },
 
-  addCommands() {
-    return {
-      setTitle:
-        (title: string) =>
-        ({ commands }: CommandProps) => {
-          return commands.setNode("title", { title });
-        },
-    };
-  },
+  // addCommands() {
+  //   return {
+  //     setTitle:
+  //       (title: string) =>
+  //       ({ commands }: CommandProps) => {
+  //         return commands.setNode("title", { title });
+  //       },
+  //   };
+  // },
 });
