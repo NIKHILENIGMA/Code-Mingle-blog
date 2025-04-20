7a57f11e4762ea7bb32d17a3d097b6740a418f89
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-04-20 23:44:38 +0530

    Refactor: Change folder and file structure to scale application better for testing purpose

--- Server/src/middleware/index.ts
+++
@@ -1,6 +0,0 @@
-import { isAuthenticated } from './authentication.middleware'
-import { validateParams, validateBody, validateQuery } from './validateSchema.middleware'
-import globalErrorHandler from './globalErrorHandler'
-import { notFound } from './NotFound.middleware'
-
-export { isAuthenticated, validateParams, validateBody, validateQuery, globalErrorHandler, notFound }
