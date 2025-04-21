92458c2df266f7806788bdfc2aa7201b367bde55
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-04-22 01:44:00 +0530

    refactor: streamline server shutdown process and remove unused database connection code

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -1,43 +1,60 @@
 import app from './app'
 import { appConfig } from './config'
-import logger, { winstonStream } from './utils/logger'
-import prisma from './config/prisma.config'
-import morgan from 'morgan'
+import logger from './utils/logger'
+import { disconnectPrisma } from './config/prisma.config'
+import 'module-alias/register';
 
-const morganFormat = ':method :url :status :response-time ms'
-
-app.use(morgan(morganFormat, { stream: winstonStream })) // Logging HTTP requests
 
 const server = app.listen(appConfig.PORT, () => {
     logger.info(`Server running on port ${appConfig.PORT}`)
 })
 
-// Connect to the database
-void (() => {
+// Graceful shutdown
+const shutdown = async (): Promise<void> => {
     try {
-        logger.info('Database connection successful')
+        await disconnectPrisma()
+        server.close(() => {
+            logger.info('🛑 Server shut down gracefully')
+            process.exit(0)
+        })
     } catch (error) {
-        logger.error('Error while connecting to the database: ', error)
+        logger.error('❌ Error during shutdown: %s', error)
         process.exit(1)
     }
-})()
+}
 
+process.on('SIGINT', () => {
+    void shutdown()
+})
+process.on('SIGTERM', () => {
+    void shutdown()
+})
+
+// Handle uncaught errors globally
+process.on('unhandledRejection', (reason) => {
+    logger.error('Unhandled Rejection:', reason)
+})
+
+process.on('uncaughtException', (error) => {
+    logger.error('Uncaught Exception:', error)
+    process.exit(1)
+})
 // Handle graceful shutdown
 // Graceful shutdown
-process.on('beforeExit', () => {
-    ;(async () => {
-        try {
-            await prisma.$disconnect() // Disconnect Prisma client
-            server.close() // Close the server
-            logger.info('Server shutting down')
-        } catch (error) {
-            logger.error('Error shutting down server: %s', error)
-            process.exit(1) // Exit with failure code
-        }
-    })().catch((err) => {
-        logger.error('Error during shutdown process: %s', err)
-        process.exit(1) // Exit with failure code
-    })
-})
+// process.on('beforeExit', () => {
+//     ;(async () => {
+//         try {
+//             await prisma.$disconnect() // Disconnect Prisma client
+//             server.close() // Close the server
+//             logger.info('Server shutting down')
+//         } catch (error) {
+//             logger.error('Error shutting down server: %s', error)
+//             process.exit(1) // Exit with failure code
+//         }
+//     })().catch((err) => {
+//         logger.error('Error during shutdown process: %s', err)
+//         process.exit(1) // Exit with failure code
+//     })
+// })
 
 export default server
