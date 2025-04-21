9bebef42196aac48f05f1597f02ce490fc29b4e9
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-04-21 21:35:12 +0530

    refactor: update route imports and middleware organization in app.ts

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -1,60 +1,59 @@
 import express, { Application } from 'express'
-import routes from './routes'
+import V1Routes from './api/routes'
 import cors from 'cors'
 import helmet from 'helmet'
 import cookieParser from 'cookie-parser'
 import { corsOptions } from './constant/corsOptions'
-import { globalErrorHandler, notFound } from './middleware'
+import { globalErrorHandler, notFound } from '@/api/middlewares'
 
 const app: Application = express()
 
 /**
- * Middlewares 
+ * Middlewares
  * @name helmet
  * @description Secure Express apps by setting various HTTP headers
- * 
+ *
  * @name express.json
  * @description Parse incoming request with JSON payloads
- * 
+ *
  * @name cookieParser
  * @description Parse Cookie header and populate req.cookies with an object keyed by the cookie names
- * 
+ *
  * @name express.urlencoded
  * @description Parse incoming request with URL-encoded payloads
- * 
+ *
  * @name express.static
  * @description Serve static files
- * 
+ *
  * @name cors
  * @description Enable CORS with various options
- * 
+ *
  * @name routes
  * @description API routes
- * 
+ *
  * @name notFound
  * @description 404 Handler
- * 
- * 
+ *
+ *
  * @name globalErrorHandler
  * @description Global error handler
  */
 
 // Middlewares
-app.use(helmet()) 
-app.use(express.json({limit: '16kb'}))
-app.use(express.urlencoded({ extended: true })) 
-app.use(express.static('public')) 
-app.use(cookieParser()) 
-app.use(cors(corsOptions)) 
+app.use(helmet())
+app.use(express.json({ limit: '10kb' }))
+app.use(express.urlencoded({ extended: true, limit: '10kb' }))
+app.use(express.static('public'))
+app.use(cookieParser())
+app.use(cors(corsOptions))
 
 // Routes
-app.use('/api/v1', routes)
-
-
+app.use('/api', V1Routes)
 
 // 404 Handler
 app.use(notFound)
 
+// Global Error Handler
 app.use(globalErrorHandler)
 
 export default app
