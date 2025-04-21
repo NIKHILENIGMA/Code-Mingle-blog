9b7350f591624106f2cffadbe29c87bcd22e5a9b
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-04-22 01:44:27 +0530

    refactor: add disconnectPrisma function for graceful PrismaClient disconnection

--- Server/src/config/prisma.config.ts
+++ Server/src/config/prisma.config.ts
@@ -8,8 +8,18 @@
 */
 
 
+import { logger } from '@/utils'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
+
+export const disconnectPrisma = async () => {
+    try {
+        await prisma.$disconnect()
+        logger.info('Prisma disconnected successfully')
+    } catch (err) {
+        logger.error('Error disconnecting Prisma: %s', err)
+    }
+}
 
 export default prisma
