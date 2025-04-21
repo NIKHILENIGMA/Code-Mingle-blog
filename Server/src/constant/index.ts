f93969e0a34f4773b8f4de2340a293cf6c02e07e
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-04-21 21:40:11 +0530

    refactor: remove DRAFT_STATUS enum and update exports in constants feat: add POST_NOT_FOUND response message to handle missing posts

--- Server/src/constant/index.ts
+++ Server/src/constant/index.ts
@@ -1,6 +1,5 @@
 import { EApplicationEnvironment } from './application'
 import { corsOptions } from './corsOptions'
-import { DRAFT_STATUS } from './draftStatus'
 import responseMessage from './responseMessage'
 
-export { EApplicationEnvironment, corsOptions, DRAFT_STATUS, responseMessage }
+export { EApplicationEnvironment, corsOptions, responseMessage }
