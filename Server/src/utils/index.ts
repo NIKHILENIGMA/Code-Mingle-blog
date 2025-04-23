ee0f4d035631f31b89ffbe596329fafbd1c836ab
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-04-23 15:13:02 +0530

    refactor: add AsyncHandler and resourceResolvers to exports in utils index

--- Server/src/utils/index.ts
+++ Server/src/utils/index.ts
@@ -3,5 +3,7 @@ import { ApiError } from './ApiError'
 import logger from './logger'
 import { cloundinaryUtil } from './Cloudinary'
 import { promptSelection } from './PromptSelection'
+import { AsyncHandler } from './AsyncHandler'
+import { resourceResolvers } from './ResourceResolver'
 
-export { ApiResponse, ApiError, logger, cloundinaryUtil, promptSelection }
+export { ApiResponse, ApiError, logger, cloundinaryUtil, promptSelection, AsyncHandler, resourceResolvers }
