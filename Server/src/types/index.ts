04661100e777ad9746648065cd1ed883e619eb53
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-04-23 15:17:09 +0530

    feat: add REQUESTS, RESPONSES, and APP_REQUEST exports to types index

--- Server/src/types/index.ts
+++ Server/src/types/index.ts
@@ -1,4 +1,7 @@
 import * as ENUMS from './common/enum.types'
 import * as BASE from './common/base.types'
+import * as REQUESTS from './api/requests.types'
+import * as RESPONSES from './api/responses.types'
+import * as APP_REQUEST from './extended/app-request'
 
-export { ENUMS, BASE }
+export { ENUMS, BASE, REQUESTS, RESPONSES, APP_REQUEST }
