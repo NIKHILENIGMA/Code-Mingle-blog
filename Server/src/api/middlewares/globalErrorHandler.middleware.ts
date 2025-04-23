54b0e82c91ec1a197d5bc7500708be92fba6212a
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-04-23 15:11:51 +0530

    refactor: improve global error handler with enhanced error message formatting and stack trace handling

--- Server/src/api/middlewares/globalErrorHandler.middleware.ts
+++ Server/src/api/middlewares/globalErrorHandler.middleware.ts
@@ -1,17 +1,19 @@
 import { NextFunction, Request, Response } from 'express'
-import { THttpError } from '@/types/api/requests.types'
+import { REQUESTS } from '@/types'
+import { isProduction } from '@/config'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const globalErrorHandler = (error: THttpError, _: Request, res: Response, __: NextFunction) => {
+const globalErrorHandler = (error: REQUESTS.THttpError, _: Request, res: Response, __: NextFunction) => {
     const statusCode = error.statusCode || 500 // Default to 500 if undefined
-    const message = error.message || 'Internal Server Error'
+    const message = error.message ? `Global Error: ${error.message}` : 'GE: Internal Server Error'
+    const stack = error.stack || 'No stack trace available'
 
     res.status(statusCode).json({
         success: false,
         statusCode,
-        message
+        message,
+        stack: isProduction ? undefined : stack
     })
-
 }
 
 export default globalErrorHandler
