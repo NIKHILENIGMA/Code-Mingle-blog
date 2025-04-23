8643b3ba2906aaa46057d0661d0950cc209521ee
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-04-23 16:11:13 +0530

    refactor: remove APP_REQUEST export from types index for clarity

--- Server/src/types/index.ts
+++ Server/src/types/index.ts
@@ -2,6 +2,5 @@ import * as ENUMS from './common/enum.types'
 import * as BASE from './common/base.types'
 import * as REQUESTS from './api/requests.types'
 import * as RESPONSES from './api/responses.types'
-import * as APP_REQUEST from './extended/app-request'
 
-export { ENUMS, BASE, REQUESTS, RESPONSES, APP_REQUEST }
+export { ENUMS, BASE, REQUESTS, RESPONSES }
