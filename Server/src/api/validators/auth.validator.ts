674fcf802b6e141d1c04f7bd380fdb79a91daf3c
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-05-12 00:24:37 +0530

    refactor: Update signupSchema to enforce username requirement and enhance password validation rules

--- Server/src/api/validators/auth.validator.ts
+++ Server/src/api/validators/auth.validator.ts
@@ -3,17 +3,27 @@ import { z } from 'zod'
 export const signupSchema = z.object({
     firstName: z.string().min(1, 'firstname is required').optional(),
     lastName: z.string().min(1, 'lastname is required').optional(),
-    username: z.string().min(1, 'Username is required').optional(),
+    username: z.string().min(1, 'Username is required'),
     email: z.string().email('Invalid email address'),
-    password: z.string().min(6, 'Password must be at least 6 characters long')
+    password: z
+        .string()
+        .min(6, 'Password must be at least 6 characters long')
+        .max(20, 'Password must be at most 20 characters long')
+        .refine((val) => /[a-zA-Z]/.test(val), {
+            message: 'Password must contain at least one letter'
+        })
+        .refine((val) => /[0-9]/.test(val), {
+            message: 'Password must contain at least one number'
+        })
+        .refine((val) => /[!@#$%^&*]/.test(val), {
+            message: 'Password must contain at least one special character'
+        })
 })
-
 
 export const loginSchema = z.object({
     email: z.string().email('Invalid email address'),
     password: z.string().min(6, 'Password must be at least 6 characters long')
 })
-
 
 export const forgotPasswordSchema = z.object({
     email: z.string().email('Invalid email address')
