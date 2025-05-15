b68660d664165c5d9fdf358a476935020bf24349
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-05-15 11:38:17 +0530

    refactor: Remove unused category and analytics controllers, schemas, services, and types

--- Server/src/features/admin/category/category.schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod'
-
-export const CategoryBodySchema = z.object({
-  name: z.string().min(3).max(255)
-})
-
-export const CategoryParamsSchema = z.object({
-    id: z.number()
-})
