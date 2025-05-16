0638b44f977411c362f1d5a9763293e7da2f7c4e
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-05-16 23:23:24 +0530

    refactor: Enhance authentication middleware with improved error handling and token extraction

--- Server/src/api/middlewares/authentication.middleware.ts
+++ Server/src/api/middlewares/authentication.middleware.ts
@@ -1,74 +1,87 @@
-import { Response, NextFunction } from 'express'
-import { decode } from '@/helpers/JWT'
-import { RepositoryFactory } from '@/Lib/Repositories'
-import { ProtectedRequest } from '@/types/extended/app-request'
-import { ApiError } from '@/utils/ApiError'
-import { AsyncHandler } from '@/utils/AsyncHandler'
-import responseMessage from '@/constant/responseMessage'
-import { User } from '@/Lib/Models/User'
+import { Request, Response, NextFunction } from 'express'
+import { BadRequestError, InternalServerError, UnauthorizedError } from '@/utils/Errors'
+import { AsyncHandler, tokenManagement } from '@/utils'
+import { StandardError } from '@/utils/Errors/StandardError'
+import { PrismaUserRepository } from '@/features/users/repository/PrismaUserRepository'
+import { UserDTO } from '@/features/users/authentication/auth.types'
+import { PrismaSessionRepository } from '@/features/users/repository/PrismaSessionRepository'
 
-const userRepository = RepositoryFactory.UserRepository()
-const keyStoreRepository = RepositoryFactory.KeyStoreRepository()
+const userRepository = new PrismaUserRepository()
+const sessionRepository = new PrismaSessionRepository()
 
-const { UNAUTHORIZED, INVALID_TOKEN, NOT_FOUND, INTERNAL_SERVICE } = responseMessage
-
-/**
- * Middleware to check if the user is authenticated.
- *
- * This middleware function checks for the presence of an access token in the request headers,
- * decodes the token, and verifies the user's existence in the database. If the user is authenticated,
- * the user object is attached to the request object and the next middleware function is called.
- * If authentication fails at any step, an ApiError with a 401 status code is thrown.
- *
- * @param {Request} req - The request object.
- * @param {Response} _ - The response object (not used).
- * @param {NextFunction} next - The next middleware function.
- *
- * @throws {ApiError} If the access token is missing, invalid, or the user does not exist.
- */
-
-export const isAuthenticated = AsyncHandler(async (req: ProtectedRequest, _: Response, next: NextFunction) => {
-    try {
-        const cookies = req.cookies as Record<string, string>
-
-        const accessToken = cookies['access_token']
-
-        if (!accessToken) {
-            return ApiError(new Error(UNAUTHORIZED.message), req, next, UNAUTHORIZED.code)
+const extractToken = (req: Request): string => {
+    let token = null
+    const authHeader = req.headers['authorization']
+    if (authHeader && typeof authHeader === 'string') {
+        const parts = authHeader.split(' ')
+        if (parts.length === 2 && parts[0] === 'Bearer') {
+            token = parts[1]
         }
+    }
 
-        // Decode the token
-        const decodedToken = await decode(req, next, accessToken)
+    if (!token) {
+        const cookies = req.cookies as Record<string, string>
+        token = cookies['access_token']
+        if (!token) {
+            throw new BadRequestError('Access token not found in request headers or cookies')
+        }
+    }
 
+    return token
+}
+
+export const isAuthenticated = AsyncHandler(async (req: Request, _: Response, next: NextFunction) => {
+    try {
+        // Check if the request has an authorization header
+        const accessToken = extractToken(req)
+
+        // Check if the access token is valid
+        const decodedToken = await tokenManagement.verifyToken(accessToken)
         if (!decodedToken) {
-            return ApiError(new Error(INVALID_TOKEN.message), req, next, INVALID_TOKEN.code)
+            throw new UnauthorizedError('Invalid access token')
         }
 
         // Find user by id
-        const user: User | null = await userRepository.findUserById({ id: decodedToken?.subject })
-
+        const user: UserDTO | null = await userRepository.getUserById(decodedToken.id)
         if (!user) {
-            return ApiError(new Error(NOT_FOUND('User').message), req, next, NOT_FOUND().code)
+            throw new BadRequestError('User not found')
         }
 
         // Attach user to request object
-        req.user = user
-        // Find key store by user id
-        const keyStore = await keyStoreRepository.findKeyStoreByUserId(user.id)
-
-        if (!keyStore) {
-            return ApiError(new Error('Unauthorized, keystore not found'), req, next, 401)
+        req.user = {
+            id: user.id,
+            email: user.email,
+            roleId: user.roleId!,
+            lastLoginAt: user.lastLoginAt!
         }
 
-        if (keyStore.accessKey !== decodedToken?.param) {
-            return ApiError(new Error('Unauthorized, user not found'), req, next, 401)
+        // Check if session is valid
+        const session = await sessionRepository.getSessionByUserId(user.id)
+        if (!session) {
+            throw new UnauthorizedError('Session not found')
         }
 
-        // Attach key store to request object
-        req.keyStore = keyStore
+        if (session.userId !== user.id) {
+            throw new UnauthorizedError('Session does not belong to the user')
+        }
+
+        // Attach session to request object
+        req.session = {
+            id: session?.id,
+            userId: session?.userId,
+            accessToken: session?.accessToken,
+            refreshToken: session?.refreshToken,
+            valid: session?.valid,
+        }
+
+        // req.perms = decodedToken.permission
 
         next()
     } catch (error) {
-        return ApiError(error instanceof Error ? error : new Error(INTERNAL_SERVICE('Authentication').message), req, next, INTERNAL_SERVICE().code)
+        if (error instanceof StandardError) {
+            throw error
+        }
+
+        throw new InternalServerError('An unexpected error occurred during authentication')
     }
 })
