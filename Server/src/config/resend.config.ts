48ac2b42fbfc32f9f74c66f87c69beb525c0d81b
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-05-16 23:28:16 +0530

    refactor: Simplify resend configuration by exporting the API key directly

--- Server/src/config/resend.config.ts
+++ Server/src/config/resend.config.ts
@@ -3,6 +3,5 @@ import dotenvFlow from 'dotenv-flow'
 dotenvFlow.config()
 
 // configMailService
-export const resendConfig = {
-    resendApiKey: process.env.RESEND_MAIL_SERVICE_API_KEY
-}
+
+export const resendApiKey = process.env.RESEND_MAIL_SERVICE_API_KEY
