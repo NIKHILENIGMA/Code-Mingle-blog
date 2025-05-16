12ebfec726161e48863a1ab1f4a6f7bec6b8b08f
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-05-16 23:30:23 +0530

    feat: Implement AuthorizationService singleton pattern for user authentication

---
+++ Server/src/features/users/authentication/authorization.service.ts
@@ -0,0 +1,20 @@
+// import prisma from '@/config/prisma.config'
+// import { UnauthorizedError } from '@/utils/Errors'
+// import { Resource } from '@prisma/client'
+
+export class AuthorizationService {
+    private static instance: AuthorizationService
+
+    private constructor() {
+        // Private constructor to prevent instantiation
+    }
+
+    public static getInstance(): AuthorizationService {
+        if (!AuthorizationService.instance) {
+            AuthorizationService.instance = new AuthorizationService()
+        }
+        return AuthorizationService.instance
+    }
+    
+    
+}
