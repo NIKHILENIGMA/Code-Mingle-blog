7ac5dc1f4a0e7e29f6df6b806cfee87e7c95249c
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-06-06 23:57:01 +0530

    refactor: Clean up documentation in checkLikeStatusService by removing unnecessary blank lines

--- Server/src/features/post/Like/like.service.ts
+++ Server/src/features/post/Like/like.service.ts
@@ -18,23 +18,23 @@ export default class LikeServices {
     /**
      * The function `checkLikeStatusService` asynchronously checks the like status of a post using
      * Prisma and returns a boolean value or throws an error.
-     * 
+     *
      * @param {Request} req - The `req` parameter in the `checkLikeStatusService` function is typically
      * used to represent the incoming request object in an Express.js application. It contains
      * information about the HTTP request being made, such as headers, parameters, and body data. This
      * parameter allows the function to access and interact with the
      * request object and its properties.
-     * 
+     *
      * @param {NextFunction} next - The `next` parameter in the `checkLikeStatusService` function is a
      * reference to the next middleware function in the Express middleware stack. It is a callback
      * function that is called when the current middleware function completes its execution. The `next`
      * function is used to pass control to the next middleware function in the stack.
-     * 
+     *
      * @param {LikeWhere} where - The `where` parameter in the `checkLikeStatusService` function is
      * used to specify the conditions for finding a like record in the database. It is passed to the
      * `prisma.like.findFirst` method to determine which like record to retrieve based on the specified
      * conditions. The `where` parameter
-     * 
+     *
      * @returns The `checkLikeStatusService` function returns a Promise that resolves to a boolean
      * value (`true` or `false`) indicating whether a like status exists based on the provided `where`
      * condition. If an error occurs during the process, an `ApiError` object is returned.
