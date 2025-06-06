8720a86019d5f11169311657a6434fad32d90d28
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-06-06 23:57:32 +0530

    refactor: Remove unused repository methods from RepositoryFactory for improved clarity

--- Server/src/Lib/Repositories/index.ts
+++ Server/src/Lib/Repositories/index.ts
@@ -1,39 +1,8 @@
-import { IUserRepository } from './Interfaces/IUserRepository'
-import { IKeyStoreRepository } from './Interfaces/IKeyStore'
-import { IResetPasswordRepository } from './Interfaces/IResetPasswordRepository'
-import { PrismaUserRepository } from './PrismaRepos/PrismaUserRepository'
-import { PrismaResetPasswordRepository } from './PrismaRepos/PrismaResetPasswordRepository'
-import { PrismaKeyStoreRepository } from './PrismaRepos/PrismaKeyStoreRepository'
-import { PrismaDraftRepository } from './PrismaRepos/PrismaDraftRepository'
-import { IDraftRepository } from './Interfaces/IDraftRepository'
 import { ICommentRepository } from './Interfaces/ICommentRepository'
 import { PrismaCommentRepository } from './PrismaRepos/PrismaCommentRepository'
-import { IReplyRepository } from './Interfaces/IReplyRepository'
-import { PrismaReplyRepository } from './PrismaRepos/PrismaReplyRepository'
 
 export class RepositoryFactory {
-    static UserRepository(): IUserRepository {
-        return new PrismaUserRepository()
-    }
-
-    static KeyStoreRepository(): IKeyStoreRepository {
-        return new PrismaKeyStoreRepository()
-    }
-
-    static ResetPasswordRepository(): IResetPasswordRepository {
-        return new PrismaResetPasswordRepository()
-    }
-
-    static DraftRepository(): IDraftRepository {
-        return new PrismaDraftRepository()
-    }
-
     static CommentRepository(): ICommentRepository {
         return new PrismaCommentRepository()
     }
-
-    static ReplyRepository(): IReplyRepository {
-        return new PrismaReplyRepository()
-    }
-    
 }
