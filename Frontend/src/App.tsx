0c631e88130ab26666fd22066426d9398cb3e568
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-06-09 01:24:40 +0530

    refactor: Move AuthProvider to wrap ThemeProvider for improved context management

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,14 +2,17 @@ import AppRouter from "./app/Router";
 import { ThemeProvider } from "@/components/DarkMode/theme-provider";
 import { QueryClientProvider } from "@tanstack/react-query";
 import queryClient from "./Utils/queryClient";
+import { AuthProvider } from "./components/common/AuthContext";
 
 function App(): JSX.Element {
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-          <AppRouter />
-        </ThemeProvider>
+        <AuthProvider>
+          <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <AppRouter />
+          </ThemeProvider>
+        </AuthProvider>
       </QueryClientProvider>
     </>
   );
