5f71a15f73e58875354b33c1b5bb04457c1b4009
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-06-09 01:26:09 +0530

    feat: Refactor AllPostsPage layout with Container and enhance search functionality

--- Frontend/src/features/Blog/pages/AllPostsPage.tsx
+++ Frontend/src/features/Blog/pages/AllPostsPage.tsx
@@ -1,7 +1,8 @@
 import { FC } from "react";
-import FilterSidebar from "../components/BlogImages/FilterSidebar";
 import { useNavigate } from "react-router-dom";
 import { dummyPosts } from "@/constants/constants";
+import Container from "@/container/Container";
+import { Button, Input } from "@/components";
 // import { getAllPostsService } from "@/services/api/postApiServices";
 // import { useQuery } from "@tanstack/react-query";
 
@@ -38,108 +39,180 @@ const AllPostsPage: FC = () => {
   };
 
   return (
-    <div className="p-4 md:p-8 max-w-7xl mx-auto my-16">
-      {/* Header */}
-      <h1 className="text-3xl md:text-4xl font-bold text-center mb-6">
-        Discover the latest stories and news
-      </h1>
+    <Container>
+      <div className="w-full p-4 my-16 md:p-8 bg-background">
+        {/* Header */}
+        <h1 className="mb-6 text-3xl font-bold text-center md:text-4xl">
+          Explore Community Stories
+        </h1>
 
-      {/* Search Bar */}
-      <div className="flex justify-center mb-8">
-        <input
-          type="text"
-          placeholder="Search blogs, stories and more..."
-          className="w-full md:w-2/3 px-4 py-2 border border-primary/40 rounded-lg focus:outline-none focus:ring-2 focus:ring-primary/50 transition duration-200 bg-background"
-        />
-      </div>
+        {/* Search Bar */}
+        <div className="relative flex justify-center mb-8">
+          <Input
+            type="text"
+            placeholder="Search blogs, stories and more..."
+            className="md:w-[30%] w-full"
+          />
+          {/* <div className="absolute flex items-center justify-center w-8 h-8 text-white rounded-full cursor-pointer bottom-2 right-2 bg-primary hover:bg-primary/80"></div> */}
+        </div>
 
-      <div className="flex flex-col md:flex-row gap-6">
-        {/* Sidebar Filters */}
-        <FilterSidebar />
+        <div className="flex items-center justify-center w-full mb-6 space-x-4">
+          <Button variant={"outline"} className="w-full rounded-full md:w-auto">
+            Followings
+          </Button>
+          <Button variant={"outline"} className="w-full rounded-full md:w-auto">
+            Popular
+          </Button>
+          <Button variant={"outline"} className="w-full rounded-full md:w-auto">
+            Latest
+          </Button>
+        </div>
 
-        {/* Posts List */}
-        <div className="w-full md:w-3/4 space-y-6">
-          {dummyPosts?.map((post: PostInterface) => (
-            <div
-              key={post.id}
-              className="flex flex-col md:flex-row bg-background text-muted-foreground rounded-lg p-4 md:p-6 space-y-4 md:space-y-0 md:space-x-6 transition hover:shadow-md cursor-pointer"
-              onClick={() => handleReadPost(post.id.toString())}
-            >
-              {/* Post Image */}
-              <div className="w-full md:w-1/3">
-                <img
-                  src={post.image}
-                  alt="Post"
-                  className="w-full h-full rounded-lg object-cover"
-                />
-              </div>
+        <div className="flex flex-col items-center justify-center w-full gap-6">
+          {/* Sidebar Filters */}
+          {/* <FilterSidebar /> */}
+          <div className="w-full "></div>
 
-              {/* Post Content */}
-              <div className="flex flex-col justify-between w-full">
-                {/* Post Header */}
-                <div>
-                  <h2 className="text-xl font-sexmibold  hover:underline cursor-pointer">
-                    {post.title}
-                  </h2>
-
-                  <div className="flex items-center text-sm text-muted-foreground/70 mt-2 space-x-3">
-                    <span>{post.date}</span>
-                    <span className="bg-primary text-muted-foreground px-2 py-1 rounded-md text-xs font-medium">
-                      {post.category}
-                    </span>
+          {/* Posts List */}
+          <div className="flex ">
+            <div className="w-full space-y-6 md:w-[80%] px-10">
+              {dummyPosts?.map((post: PostInterface) => (
+                <div
+                  key={post.id}
+                  className="w-full p-4 space-y-4 transition rounded-lg cursor-pointer border-[1px] sm:flex md:flex-row bg-background text-muted-foreground md:p-6 md:space-y-0 md:space-x-6 hover:shadow-md border-secondary/85 hover:bg-secondary/15"
+                  onClick={() => handleReadPost(post.id.toString())}
+                >
+                  {/* Post Image */}
+                  <div className="w-full md:w-1/2">
+                    <img
+                      src={post.image}
+                      alt="Post"
+                      className="object-cover w-full h-full rounded-lg"
+                    />
                   </div>
 
-                  <p className="text-muted-foreground/50 mt-3 text-sm md:text-base">
-                    {post.description}
-                  </p>
-                </div>
+                  {/* Post Content */}
+                  <div className="flex flex-col justify-between w-full">
+                    {/* Post Header */}
+                    <div>
+                      <h2 className="text-xl cursor-pointer font-sexmibold hover:underline">
+                        {post.title}
+                      </h2>
 
-                {/* Author Section */}
-                <div className="flex items-center mt-4 space-x-3">
+                      <div className="flex items-center mt-2 space-x-3 text-sm text-muted-foreground/70">
+                        <span>{post.date}</span>
+                        <span className="px-2 py-1 text-xs font-medium rounded-md bg-primary text-muted-foreground">
+                          {post.category}
+                        </span>
+                      </div>
+
+                      <p className="mt-3 text-sm text-muted-foreground/50 md:text-base">
+                        {post.description}
+                      </p>
+                    </div>
+
+                    {/* Author Section */}
+                    <div className="flex items-center mt-4 space-x-3">
+                      <img
+                        src="https://randomuser.me/api/portraits"
+                        alt="Author"
+                        className="object-cover w-10 h-10 rounded-full"
+                      />
+                      <div>
+                        <h4 className="text-sm font-semibold text-muted-foreground">
+                          {post.author}
+                        </h4>
+                        <p className="text-xs text-muted-foreground/60">
+                          {post.role}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+
+              {/* Pagination */}
+              <div className="flex justify-center gap-2 mt-6">
+                <button className="text-purple-600 hover:underline">
+                  Previous
+                </button>
+                {[1, 2, 3, 4, 5].map((num) => (
+                  <button
+                    key={num}
+                    className={`px-3 py-1 rounded-full ${
+                      num === 1
+                        ? "bg-purple-600 text-white"
+                        : "border border-gray-300 text-gray-600 hover:bg-gray-100"
+                    }`}
+                  >
+                    {num}
+                  </button>
+                ))}
+                <button className="px-3 py-1 text-gray-600 border rounded-full">
+                  ...
+                </button>
+                <button className="px-3 py-1 text-gray-600 border rounded-full">
+                  100
+                </button>
+                <button className="text-purple-600 hover:underline">
+                  Next
+                </button>
+              </div>
+            </div>
+            <div className="hidden md:w-[20%] md:block md:h-[80%] border-secondary/85 border shadow-sm pl-4 rounded-lg">
+              <div className="h-full p-4 space-y-6 ">
+                <h3 className="font-medium text-wrap ">Top Writer ✨</h3>
+                <div className="flex items-center space-x-3 cursor-pointer">
                   <img
-                    src="https://randomuser.me/api/portraits"
-                    alt="Author"
-                    className="w-10 h-10 rounded-full object-cover"
+                    src="https://images.unsplash.com/photo-1503104834685-7205e8607eb9?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    alt="Top Writer"
+                    className="object-cover w-10 h-10 rounded-full"
                   />
                   <div>
                     <h4 className="text-sm font-semibold text-muted-foreground">
-                      {post.author}
+                      John Doe
                     </h4>
-                    <p className="text-xs text-muted-foreground/60">{post.role}</p>
+                    <p className="text-xs text-muted-foreground/60">
+                      1000+ Followers
+                    </p>
+                  </div>
+                </div>
+                <div className="flex items-center space-x-3">
+                  <img
+                    src="https://images.unsplash.com/photo-1503104834685-7205e8607eb9?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    alt="Top Writer"
+                    className="object-cover w-10 h-10 rounded-full"
+                  />
+                  <div>
+                    <h4 className="text-sm font-semibold text-muted-foreground">
+                      John Doe
+                    </h4>
+                    <p className="text-xs text-muted-foreground/60">
+                      1000+ Followers
+                    </p>
+                  </div>
+                </div>
+                <div className="flex items-center space-x-3">
+                  <img
+                    src="https://images.unsplash.com/photo-1503104834685-7205e8607eb9?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    alt="Top Writer"
+                    className="object-cover w-10 h-10 rounded-full"
+                  />
+                  <div>
+                    <h4 className="text-sm font-semibold text-muted-foreground">
+                      John Doe
+                    </h4>
+                    <p className="text-xs text-muted-foreground/60">
+                      1000+ Followers
+                    </p>
                   </div>
                 </div>
               </div>
             </div>
-          ))}
-
-          {/* Pagination */}
-          <div className="flex justify-center gap-2 mt-6">
-            <button className="text-purple-600 hover:underline">
-              Previous
-            </button>
-            {[1, 2, 3, 4, 5].map((num) => (
-              <button
-                key={num}
-                className={`px-3 py-1 rounded-full ${
-                  num === 1
-                    ? "bg-purple-600 text-white"
-                    : "border border-gray-300 text-gray-600 hover:bg-gray-100"
-                }`}
-              >
-                {num}
-              </button>
-            ))}
-            <button className="text-gray-600 px-3 py-1 border rounded-full">
-              ...
-            </button>
-            <button className="text-gray-600 px-3 py-1 border rounded-full">
-              100
-            </button>
-            <button className="text-purple-600 hover:underline">Next</button>
           </div>
         </div>
       </div>
-    </div>
+    </Container>
   );
 };
 
