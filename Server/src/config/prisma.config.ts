d2d230dff1d1c8a36e4e15d4a170a0cddddedd45
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-06-11 12:31:37 +0530

    refactor: Remove unnecessary comments from prisma.config.ts for clarity

--- Server/src/config/prisma.config.ts
+++ Server/src/config/prisma.config.ts
@@ -1,11 +1,4 @@
-/*  
-@desciption: This file is responsible for creating a new instance of the PrismaClient class and exporting it for use in other parts of the codebase.
 
-* This code snippet is importing the `PrismaClient` class from the `@prisma/client` package. 
-* It then creates a new instance of `PrismaClient` called `prisma`. 
-* Finally, it exports this `prisma` instance, making it available for use in other parts of the codebase. 
-* This setup allows you to interact with a Prisma client for database operations in your TypeScript project. 
-*/
 
 import { logger } from '@/utils/logger/index'
 import { PrismaClient } from '@prisma/client'
