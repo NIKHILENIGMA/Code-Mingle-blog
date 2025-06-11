02cc84459b70f82cac3d9803dc0995f1dc63c244
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-06-11 12:31:50 +0530

    refactor: Restructure Cloudinary configuration for improved clarity and organization

--- Server/src/config/cloudinary.config.ts
+++ Server/src/config/cloudinary.config.ts
@@ -1,7 +1,10 @@
-export const fileUploadConfig = {
-    FILE_SIZE_LIMIT: parseInt(process.env.FILE_UPLOAD_MAX_SIZE || '5242880'), // 5MB
-    FILE_UPLOAD_PATH: (process.env.FILE_UPLOAD_PATH as string) || './public/uploads'
-}
+// Description: Configuration for Cloudinary and file upload settings
+import dotenvFlow from 'dotenv-flow'
+
+dotenvFlow.config()
+
+export const FILE_SIZE_LIMIT: number = parseInt(process.env.FILE_UPLOAD_MAX_SIZE || '5242880') // max 5MB for file uploads
+export const FILE_UPLOAD_PATH: string = process.env.FILE_UPLOAD_PATH || './public/uploads' // default upload path
 
 export const cloudinaryConfig = {
     CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME as string,
@@ -9,9 +12,13 @@ export const cloudinaryConfig = {
     API_SECRET: process.env.CLOUDINARY_API_SECRET as string
 }
 
+export const CLOUDINARY_CLOUD_NAME: string = process.env.CLOUDINARY_CLOUD_NAME || 'CLOUDINARY_CLOUD_NAME'
+export const CLOUDINARY_API_KEY: string = process.env.CLOUDINARY_API_KEY || 'API_KEY'
+export const CLOUDINARY_API_SECRET: string = process.env.CLOUDINARY_API_SECRET || 'API_SECRET'
+
 export const ALLOWED_FORMATS: string[] = ['jpg', 'png', 'jpeg']
-export const MAX_BYTES = fileUploadConfig.FILE_SIZE_LIMIT
-export const FORMAT = 'webp'
+export const MAX_BYTES = FILE_SIZE_LIMIT
+export const FORMAT = 'webp' 
 export const QUALITY: number = 100
 export const RESOURCE_TYPE: 'image' | 'auto' | 'raw' | 'video' = 'image'
 export const OVERWRITE = true
