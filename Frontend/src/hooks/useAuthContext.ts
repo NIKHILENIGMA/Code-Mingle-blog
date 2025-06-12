34af0a3cddb0903c6cf71960ac20d7d8b2343298
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-06-12 12:51:45 +0530

    feat: Add useAuthContext hook for authentication context management

---
+++ Frontend/src/hooks/useAuthContext.ts
@@ -0,0 +1,11 @@
+import { AuthContext } from "@/components/common/AuthContext";
+import { useContext } from "react";
+
+export const useAuthContext = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuthContext must be used within an AuthProvider");
+  }
+
+  return context;
+};
