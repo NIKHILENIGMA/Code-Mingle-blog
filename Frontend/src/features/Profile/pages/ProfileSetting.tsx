b46d4d9a05df3c39af775eb1f86573f367363dd3
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-06-17 12:43:47 +0530

    refactor: Update import for sidebar options and improve layout consistency in ProfileSettings component

--- Frontend/src/features/Profile/pages/ProfileSetting.tsx
+++ Frontend/src/features/Profile/pages/ProfileSetting.tsx
@@ -1,17 +1,17 @@
-import { settingsSidebarOptions } from "@/constants/ProfileContants";
+import { SETTINGS_SIDEBAR_OPTIONS } from "@/constants";
 import { FC } from "react";
 import { NavLink, Outlet } from "react-router-dom";
 
 const ProfileSettings: FC = () => {
   return (
-    <div className="flex flex-col md:flex-row bg-gray-100 dark:bg-gray-900 min-h-screen p-6 mt-16">
+    <div className="flex flex-col min-h-screen p-6 mt-16 bg-gray-100 md:flex-row dark:bg-gray-900">
       {/* Sidebar */}
-      <aside className="w-full md:w-1/5 bg-white p-6 rounded-lg shadow-md">
+      <aside className="w-full p-6 bg-white rounded-lg shadow-md md:w-1/5">
         <h2 className="text-lg font-semibold text-gray-700">
           General Settings
         </h2>
         <ul className="mt-4 space-y-3 text-gray-600">
-          {settingsSidebarOptions.map((option, index) => (
+          {SETTINGS_SIDEBAR_OPTIONS.map((option, index) => (
             <li key={`${index}-${option}`}>
               <NavLink
                 to={option.path}
