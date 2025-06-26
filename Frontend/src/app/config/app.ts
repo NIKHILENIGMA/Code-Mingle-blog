e25707a0d1bd5e0fb8a088183ba7fe9d2532136b
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-06-26 17:10:13 +0530

    feat: Add configuration for environment variables and Google OAuth 2.0

---
+++ Frontend/src/app/config/app.ts
@@ -0,0 +1,21 @@
+
+type ResponseType = 'code'
+enum ENIRONMENT {
+    DEVELOPMENT = 'DEVELOPMENT', 
+    PRODUCTION = 'PRODUCTION'
+}
+// Application
+export const NODE_ENV: string = import.meta.env.NODE_ENV || 'development'
+export const SERVER_BASE_URL= import.meta.env.SERVER_BASE_URL || 'http://localhost:5000'
+export const IS_DEVELOPMENT: boolean = import.meta.env.NODE_ENV === ENIRONMENT.DEVELOPMENT
+export const IS_PRODUCTION: boolean = import.meta.env.NODE_ENV === ENIRONMENT.PRODUCTION
+
+
+// Google OAuth 2.0 
+export const GOOGLE_CLIENT_ID: string = import.meta.env.VITE_GOOGLE_CLIENT_ID
+export const GOOGLE_REDIRECT_URI: string = import.meta.env.VITE_GOOGLE_REDIRECT_URI
+export const GOOGLE_SCOPE: string = 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile'
+export const GOOGLE_ACCESS_TYPE: string = 'offline'
+export const GOOGLE_RESPONSE_TYPE: ResponseType = 'code' 
+export const GOOGLE_PROMPT: string = 'consent'
+
