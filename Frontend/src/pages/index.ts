571fd3307badffe7dbeb1a34947c2255d06da834
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-07-05 21:40:13 +0530

    feat: Remove unused LoginPage and ResetPasswordPage imports from index

--- Frontend/src/pages/index.ts
+++ Frontend/src/pages/index.ts
@@ -1,8 +1,6 @@
 import HomePage from "./Public/HomePage";
 import AboutPage from "./Public/AboutPage";
 import NotFoundPage from "./Public/NotFoundPage";
-import LoginPage from "./Auth/LoginPage";
-import ResetPasswordPage from "./Auth/ForgotPasswordPage";
 // import SignupPage from "./Auth/SignupPage";
 import ReadPostPage from "@/pages/Blog/ReadPostPage";
 import LearnEditorPage from "@/pages/Blog/LearnEditorPage";
@@ -11,8 +9,6 @@ export {
   HomePage,
   AboutPage,
   NotFoundPage,
-  LoginPage,
-  ResetPasswordPage,
   // SignupPage,
   ReadPostPage,
   // PreviewPostPage,
