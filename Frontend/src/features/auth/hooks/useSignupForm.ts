bd71565435c72f0084013250c25c1a930498a762
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-08-05 18:21:58 +0530

    feat: implement custom hook for signup form handling

---
+++ Frontend/src/features/auth/hooks/useSignupForm.ts
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import { authService } from '../services/authApiServices'
+import { ApiError } from '@/services/api/apiClient'
+import { toast } from 'sonner'
+import { SignupRequest } from '../types/authTypes'
+import { useNavigate } from 'react-router-dom'
+
+export const useSignupForm = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const navigate = useNavigate()
+
+  const signup = async (data: SignupRequest) => {
+    setIsLoading(true)
+    try {
+      await authService.signup(data)
+      // Show success message
+      toast.success('Signup successful! Please log in to continue.')
+      // Navigate to the desired page after successful signup
+      navigate('/login')
+    } catch (err) {
+      const apiResponse = err as ApiError
+      toast.error(
+        apiResponse.message ||
+          'Login failed. Please check your credentials and try again.',
+      )
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  return { isLoading, signup }
+}
