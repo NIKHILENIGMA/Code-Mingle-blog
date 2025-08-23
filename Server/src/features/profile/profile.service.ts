a46b060220f29a53a722dafc9bb9be35f09baadc
Nikhil Harmalkar <nickharmalkar18@gmail.com>
2025-08-23 12:44:23 +0530

    feat: implement profile management features including user details update, password change, avatar and cover photo upload, and validation schemas

---
+++ Server/src/features/profile/profile.service.ts
@@ -0,0 +1,98 @@
+import { IUserRepository, PrismaUserRepository } from '@/features/authentication/PrismaUserRepository'
+import { StandardError } from '@/utils/Errors/StandardError'
+import { BadRequestError, DatabaseError, InternalServerError } from '@/utils/Errors'
+import { Dashboard, ProfileChangePasswordCredentials, UpdateProfileCredentials } from './profile.types'
+import { User } from '@/generated/prisma/client'
+import { comparePassword, hashedPassword } from '@/utils/HashPassword'
+
+class ProfileService {
+    constructor(private userRepository: IUserRepository) {
+        this.userRepository = userRepository
+    }
+
+    public async updateUserProfileService(id: string, userDetails: UpdateProfileCredentials): Promise<void> {
+        try {
+            await this.userRepository.updateUserDetails(id, userDetails)
+        } catch (error) {
+            if (error instanceof StandardError) {
+                throw error
+            }
+
+            throw new InternalServerError('An error occurred while updating the user profile')
+        }
+    }
+
+    public async deleteAccountService(id: string): Promise<User | void> {
+        try {
+            await this.userRepository.delete(id)
+        } catch (error) {
+            if (error instanceof StandardError) {
+                throw error
+            }
+
+            throw new InternalServerError('An error occurred while deleting the user profile')
+        }
+    }
+
+    public async changePasswordService(id: string, passwords: ProfileChangePasswordCredentials): Promise<void> {
+        // Update user password
+        try {
+            // Check if the old password is correct
+            const user = await this.userRepository.getUserById(id)
+            if (!user) throw new BadRequestError('User not found')
+
+            // if no password found then throw error
+            if (!user.password) throw new BadRequestError('User password not found')
+
+            // Compare the old password with the hashed password
+            await comparePassword(passwords.oldPassword, user.password)
+
+            // Hash the new password
+            const hashPassword = await hashedPassword(passwords.newPassword)
+            if (!hashPassword) throw new BadRequestError('Error hashing password')
+
+            // Update the password in the database
+            await this.userRepository.updatePassword(id, hashPassword, user.password)
+        } catch (error) {
+            if (error instanceof StandardError) {
+                throw error
+            }
+
+            throw new InternalServerError('An error occurred while changing the user password')
+        }
+    }
+
+    public async getProfileDetailsService(userId: string): Promise<Dashboard> {
+        try {
+            const dashboard = await this.userRepository.getDashboardDetails(userId)
+
+            if (!dashboard) {
+                throw new DatabaseError('Unable to fetch the dashboard data')
+            }
+
+            return dashboard
+        } catch (error) {
+            if (error instanceof StandardError) {
+                throw error
+            }
+
+            throw new InternalServerError('An error occurred while changing the user password')
+        }
+    }
+
+    public async getFollowingStatus(viewerId: string, profileId: string): Promise<boolean> {
+        try {
+            const followingStatus = await this.userRepository.getFollowingStatus(viewerId, profileId)
+            return followingStatus
+        } catch (error) {
+            if (error instanceof StandardError) {
+                throw error
+            }
+
+            throw new InternalServerError('An error occurred while fetching user permissions')
+        }
+    }
+}
+
+const profileService = new ProfileService(new PrismaUserRepository())
+export default profileService
